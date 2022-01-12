90817251cacdcd7ce79401e17e1a7c327ea87711
julian-perge <julianperge@gmail.com>
2022-01-12 17:21:57 -0500

    Cleaned up MapNodeManagerPatches.cs

--- Core/MapNodeManagerPatches.cs
+++ Core/MapNodeManagerPatches.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using DiskCardGame;
+﻿using DiskCardGame;
 using HarmonyLib;
 
 namespace GrimoraMod
@@ -7,48 +6,6 @@ namespace GrimoraMod
 	[HarmonyPatch(typeof(MapNodeManager))]
 	public class MapNodeManagerPatches
 	{
-		// [HarmonyPrefix, HarmonyPatch(nameof(MapNodeManager.DoMoveToNewNode))]
-		// public static void Prefix(ref MapNodeManager __instance, out MapNodeManager __state)
-		// {
-		// 	__state = __instance;
-		// }
-
-		[HarmonyPostfix, HarmonyPatch(nameof(MapNodeManager.DoMoveToNewNode))]
-		public static IEnumerator Postfix(IEnumerator enumerator, MapNodeManager __instance, MapNode newNode)
-		{
-			GrimoraPlugin.Log.LogDebug($"[MapNodeManager.DoMoveToNewNode][Postfix] State [{__instance}] MapNode [{newNode}]");
-			__instance.MovingNodes = true;
-			__instance.SetAllNodesInteractable(false);
-			__instance.transitioningGridY = newNode.Data.gridY;
-			ViewManager.Instance.Controller.LockState = ViewLockState.Locked;
-			yield return PlayerMarker.Instance.MoveToPoint(newNode.transform.position, true);
-
-			if (newNode.Data is null)
-			{
-				GrimoraPlugin.Log.LogDebug("this is indeed null");
-				newNode.Data = new NodeData();
-				newNode.Data.prefabPath = "Prefabs/Map/MapNodesPart1/MapNode_Empty";
-			}
-
-			if (newNode.Data != null)
-			{
-				GrimoraPlugin.Log.LogDebug($"[MapNodeManager.DoMoveToNewNode][Postfix] Calling OnArriveAtNode");
-				yield return newNode.OnArriveAtNode();
-			}
-
-			ViewManager.Instance.Controller.LockState = ViewLockState.Unlocked;
-			GrimoraPlugin.Log.LogDebug($"[MapNodeManager.DoMoveToNewNode][Postfix] No longer MovingNodes");
-
-			__instance.MovingNodes = false;
-
-			CustomCoroutine.WaitOnConditionThenExecute(
-				() => !GameFlowManager.Instance.Transitioning,
-				delegate { RunState.Run.currentNodeId = newNode.nodeId; }
-			);
-			GrimoraPlugin.Log.LogDebug($"[MapNodeManager.DoMoveToNewNode][Postfix] Finished setting currentNodeId");
-			yield break;
-		}
-
 		[HarmonyPrefix, HarmonyPatch(nameof(MapNodeManager.SetActiveNode))]
 		public static bool PrefixChangeActiveNodeLogic(ref MapNode newActiveNode)
 		{
