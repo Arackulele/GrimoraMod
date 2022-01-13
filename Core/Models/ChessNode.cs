451f8aa788eb16153be7839f0987783656a9e5e2
julian-perge <julianperge@gmail.com>
2022-01-13 18:20:46 -0500

    Separated out model classes.

---
+++ Core/Models/ChessNode.cs
@@ -0,0 +1,58 @@
+namespace GrimoraMod
+{
+	public class ChessNode
+	{
+		private readonly ChessRow _row;
+		private readonly int _index;
+		public int JsonValue;
+		public readonly bool IsBlocker;
+		public readonly bool isBoss;
+		public readonly bool isChest;
+		public readonly bool isEnemy;
+		public readonly bool IsPath;
+		public readonly bool isPlayer;
+
+		public int GridX => _index;
+		public int GridY => _row.Index;
+
+
+		public ChessNode(int jsonValue, int index, ChessRow row)
+		{
+			switch (jsonValue)
+			{
+				case 0:
+					IsPath = true;
+					break;
+				case 1:
+					IsBlocker = true;
+					break;
+				case 2:
+					isChest = true;
+					break;
+				case 3:
+					isEnemy = true;
+					break;
+				case 4:
+					isBoss = true;
+					break;
+				default:
+					isPlayer = true;
+					break;
+			}
+
+			this._index = index;
+			this._row = row;
+			this.JsonValue = jsonValue;
+		}
+
+		public string GetCoords()
+		{
+			return $"{_index}_{_row.Index}";
+		}
+
+		public override string ToString()
+		{
+			return IsPath ? "Path" : "isBlocker";
+		}
+	}
+}
---
+++ Core/Models/ChessRow.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GrimoraMod
+{
+	public class ChessRow
+	{
+		public readonly List<ChessNode> Columns;
+		protected internal readonly int Index;
+
+		public ChessRow(List<int> columns, int index)
+		{
+			this.Columns = columns.Select((c, idx) => new ChessNode(c, idx, this)).ToList();
+			this.Index = index;
+		}
+
+		public bool HasOpenPathNodes()
+		{
+			return this.Columns.Any(c => c.IsPath);
+		}
+
+		public List<ChessNode> GetNodesOfType(int type)
+		{
+			return this.Columns.Where(c => c.JsonValue == type).ToList();
+		}
+	}
+}
