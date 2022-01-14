74b609f94f623deabe3d49e003e13f09fd084446
julian-perge <julianperge@gmail.com>
2022-01-14 13:10:19 -0500

    Updated logic for first time entrance to now play the RevealGrimoraSequence

--- Core/GrimoraGameFlowManagerPatches.cs
+++ Core/GrimoraGameFlowManagerPatches.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using DiskCardGame;
+﻿using DiskCardGame;
 using HarmonyLib;
+using Pixelplacement;
 using UnityEngine;
 
 namespace GrimoraMod
@@ -8,76 +8,95 @@ namespace GrimoraMod
 	[HarmonyPatch(typeof(GrimoraGameFlowManager))]
 	public class GrimoraGameFlowManagerPatches
 	{
-
-
 		[HarmonyPrefix, HarmonyPatch(nameof(GrimoraGameFlowManager.SceneSpecificInitialization))]
 		public static bool PrefixAddMultipleSequencersDuringLoad(GrimoraGameFlowManager __instance)
 		{
-			if (SaveManager.SaveFile.IsGrimora)
+			// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Instance is [{__instance.GetType()}]");
+
+			// bool skipIntro = GrimoraPlugin.ConfigHasPlayedRevealSequence.Value;
+			bool setLightsActive = true;
+
+			if (FinaleDeletionWindowManager.instance != null)
 			{
-				ChangeChessboardToExtendedClass();
+				// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Destroying FinaleDeletionWindowManager as it exists");
+				Object.Destroy(FinaleDeletionWindowManager.instance.gameObject);
+			}
 
-				AddRareCardSequencerToScene();
+			ViewManager.Instance.SwitchToView(View.Default, immediate: true);
 
-				AddDeckReviewSequencerToScene();
+			if (!StoryEventsData.EventCompleted(StoryEvent.GrimoraReachedTable))
+			{
+				StoryEventsData.SetEventCompleted(StoryEvent.GrimoraReachedTable, true);
 
-				bool skipIntro = false;
-				bool skipTombstone = false;
+				GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] GrimoraReachedTable is false");
 
-				if (FinaleDeletionWindowManager.instance != null)
+				if (GameMap.Instance != null)
 				{
-					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Destroying FinaleDeletionWindowManager as it exists");
-					Object.Destroy(FinaleDeletionWindowManager.instance.gameObject);
+					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Calling GameMap.HideMapImmediate as it exists");
+					GameMap.Instance.HideMapImmediate();
 				}
 
-				ViewManager.Instance.SwitchToView(View.Default, immediate: true);
+				// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Setting __instance.CurrentGameState to GameState.FirstPerson3D");
+				__instance.CurrentGameState = GameState.FirstPerson3D;
 
-				if (!StoryEventsData.EventCompleted(StoryEvent.GrimoraReachedTable))
+				// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Transitioning to FirstPerson3D");
+				__instance.StartCoroutine(__instance.TransitionTo(GameState.FirstPerson3D, null, immediate: true));
+
+				ExplorableAreaManager.Instance.HangingLight.gameObject.SetActive(setLightsActive);
+				ExplorableAreaManager.Instance.HandLight.gameObject.SetActive(setLightsActive);
+
+				__instance.gameTableCandlesParent.SetActive(setLightsActive);
+				__instance.gravestoneNavZone.SetActive(setLightsActive);
+
+				CryptEpitaphSlotInteractable cryptEpitaphSlotInteractable =
+					Object.FindObjectOfType<CryptEpitaphSlotInteractable>();
+
+				AudioController.Instance.PlaySound3D(
+					"giant_stones_falling",
+					MixerGroup.ExplorationSFX,
+					__instance.transform.position
+				);
+
+				Tween.Position(
+					cryptEpitaphSlotInteractable.tombstoneParent,
+					cryptEpitaphSlotInteractable.tombstoneParent.position + Vector3.down * 11f,
+					6f,
+					0f,
+					Tween.EaseIn
+				);
+
+				Tween.Shake(
+					cryptEpitaphSlotInteractable.tombstoneAnim,
+					cryptEpitaphSlotInteractable.tombstoneAnim.localPosition,
+					new Vector3(0.05f, 0.05f, 0.05f),
+					0.1f,
+					0f,
+					Tween.LoopType.Loop
+				);
+				__instance.StartCoroutine((
+						(GrimoraGameFlowManager)Singleton<GameFlowManager>.Instance).RevealGrimoraSequence()
+				);
+				// else
+				// {
+				// 	// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Intro is not being skipped");
+				// 	__instance.StartCoroutine(__instance.StartSceneSequence());
+				// }
+				SaveManager.SaveToFile();
+			}
+			else
+			{
+				GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] GrimoraReachedTable is true, playing finalegrimora_ambience");
+				AudioController.Instance.SetLoopAndPlay("finalegrimora_ambience");
+				if (GameMap.Instance != null)
 				{
-					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] GrimoraReachedTable is false");
-
-					if (GameMap.Instance != null)
-					{
-						// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Calling GameMap.HideMapImmediate as it exists");
-						GameMap.Instance.HideMapImmediate();
-					}
-
-					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Setting __instance.CurrentGameState to GameState.FirstPerson3D");
-					__instance.CurrentGameState = GameState.FirstPerson3D;
-
-					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Transitioning to FirstPerson3D");
-					__instance.StartCoroutine(__instance.TransitionTo(GameState.FirstPerson3D, null, immediate: true));
-
-					ExplorableAreaManager.Instance.HangingLight.gameObject.SetActive(skipTombstone);
-					ExplorableAreaManager.Instance.HandLight.gameObject.SetActive(skipTombstone);
-
-					__instance.gameTableCandlesParent.SetActive(skipTombstone);
-					__instance.gravestoneNavZone.SetActive(skipTombstone);
-
-					if (!skipIntro)
-					{
-						// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Intro is not being skipped");
-						__instance.StartCoroutine(__instance.StartSceneSequence());
-					}
+					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Setting CurrentGameState to GameState.Map");
+					__instance.CurrentGameState = GameState.Map;
+					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Transitioning to GameState.Map");
+					__instance.StartCoroutine(__instance.TransitionTo(GameState.Map, null, immediate: true));
 				}
-				else
-				{
-					// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] GrimoraReachedTable is true, playing finalegrimora_ambience");
-					AudioController.Instance.SetLoopAndPlay("finalegrimora_ambience");
-					if (GameMap.Instance != null)
-					{
-						// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Setting CurrentGameState to GameState.Map");
-						__instance.CurrentGameState = GameState.Map;
-						// GrimoraPlugin.Log.LogDebug($"[SceneSpecificInitialization] Transitioning to GameState.Map");
-						__instance.StartCoroutine(__instance.TransitionTo(GameState.Map, null, immediate: true));
-					}
-				}
+			}
 
-				return false;
+			return false;
 		}
-
-
-
-
 	}
 }
