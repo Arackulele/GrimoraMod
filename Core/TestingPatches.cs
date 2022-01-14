92e62c51e7e7085dbc219d8596414f875cf1443c
julian-perge <julianperge@gmail.com>
2022-01-14 17:46:15 -0500

    Added patch for adding the rest of the abilities in order for them to show up.

--- Core/TestingPatches.cs
+++ Core/TestingPatches.cs
@@ -39,5 +39,47 @@ namespace GrimoraMod
 				return false;
 			}
 		}
+
+
+		[HarmonyPatch(typeof(RuleBookController))]
+		public class RuleBookControllerPatches
+		{
+			[HarmonyPrefix, HarmonyPatch(nameof(RuleBookController.Start))]
+			public static bool PrefixAddRestOfAbilityMetaCategoriesPatch(ref RuleBookController __instance)
+			{
+				if (SaveManager.SaveFile.IsGrimora)
+				{
+					List<AbilityMetaCategory> pagesToConstruct = new List<AbilityMetaCategory>()
+					{
+						AbilityMetaCategory.Part1Modular, AbilityMetaCategory.Part1Rulebook,
+						AbilityMetaCategory.GrimoraRulebook, AbilityMetaCategory.MagnificusRulebook,
+						AbilityMetaCategory.Part3Modular, AbilityMetaCategory.Part3Rulebook
+					};
+
+					List<RuleBookPageInfo> pageInfos = new List<RuleBookPageInfo>();
+					GrimoraPlugin.Log.LogDebug($"[RuleBookController.Start] About to start adding all rulebooks");
+					foreach (var category in pagesToConstruct)
+					{
+						pageInfos.AddRange(__instance.bookInfo.ConstructPageData(category));
+					}
+
+					pageInfos = pageInfos
+						.GroupBy(i => i.ability)
+						.Select(i => i.First()).ToList();
+
+					GrimoraPlugin.Log.LogDebug($"[RuleBookController.Start] Setting pages of rulebook infos");
+					__instance.PageData = pageInfos;
+
+					// RuleBookController.Instance.PageData.ForEach(info => UnityExplorer.ExplorerCore.Log(info.ability));
+
+					// AbilitiesUtil.GetAbilities(true, categoryCriteria: AbilityMetaCategory.Part1Modular)
+					// 	.ForEach(ability => UnityExplorer.ExplorerCore.Log(ability));
+
+					return false;
+				}
+
+				return true;
+			}
+		}
 	}
 }
