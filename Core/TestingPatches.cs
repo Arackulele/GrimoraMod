59efc1882ee49635d217b3b2bcc4ff595f5e8f90
julian-perge <julianperge@gmail.com>
2022-01-14 13:11:25 -0500

    Updated logic for ViewController.SwitchToControlMode prefix to now mimic the original logic with some changes for ControlMode.Map.

--- Core/TestingPatches.cs
+++ Core/TestingPatches.cs
@@ -1,74 +1,42 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using DiskCardGame;
 using HarmonyLib;
-using UnityEngine;
 
 namespace GrimoraMod
 {
 	[HarmonyPatch]
 	public class TestingPatches
 	{
-		[HarmonyPatch(typeof(GameMap))]
-		public class BaseGameMapPatches
-		{
-			[HarmonyPostfix, HarmonyPatch(nameof(GameMap.ShowMapSequence))]
-			public static IEnumerator AddLogging(IEnumerator enumerator, GameMap __instance, float unrollSpeed = 1f)
-			{
-				if (SaveManager.SaveFile.IsGrimora)
-				{
-					__instance.FullyUnrolled = false;
-					yield return __instance.UnrollingSequence(unrollSpeed);
-
-					// todo: have to set this again because for some reason it doesn't take it during transitions?
-					PlayerMarker.Instance.transform.position = MapNodeManager.Instance.ActiveNode.transform.position;
-
-
-					PlayerMarker.Instance.Show();
-
-					__instance.FullyUnrolled = true;
-					yield break;
-				}
-				else
-				{
-					yield return enumerator;
-					yield break;
-				}
-			}
-		}
-
 		[HarmonyPatch(typeof(ViewController))]
 		public class ViewControllerPatches
 		{
-			[HarmonyPostfix, HarmonyPatch(nameof(ViewController.SwitchToControlMode))]
-			public static void Postfix(ref ViewController.ControlMode mode, bool immediate = false)
+			[HarmonyPrefix, HarmonyPatch(nameof(ViewController.SwitchToControlMode))]
+			public static bool Prefix(
+				ref ViewController __instance, 
+				ref ViewController.ControlMode mode,
+				bool immediate = false)
 			{
-				if (!SaveManager.SaveFile.IsGrimora)
+				if (!SaveManager.SaveFile.IsGrimora || mode != ViewController.ControlMode.Map)
 				{
-					return;
+					return true;
 				}
-				
-				if (mode == ViewController.ControlMode.Map 
-				    && !ViewManager.Instance.controller.allowedViews.Contains(View.MapDeckReview))
+
+				__instance.controlMode = mode;
+				View currentView = ViewManager.Instance.CurrentView;
+				__instance.altTransitionInputs.Clear();
+
+				if (mode == ViewController.ControlMode.Map)
 				{
-					GrimoraPlugin.Log.LogDebug($"-> Adding MapDeckReview to allowed views");
-					ViewManager.Instance.controller.allowedViews.Add(View.MapDeckReview);
+					GrimoraPlugin.Log.LogDebug($"[ViewController.SwitchToControlMode] Adding MapDeckReview to allowed views");
+					__instance.allowedViews = new List<View> { View.MapDefault, View.MapDeckReview };
+
+					if (!__instance.allowedViews.Contains(currentView))
+					{
+						ViewManager.Instance.SwitchToView(View.MapDefault, immediate);
+					}
 				}
-			}
-		}
 
-		[HarmonyPatch(typeof(GameFlowManager))]
-		public class GameFlowManagerPatches
-		{
-
-		}
-
-		[HarmonyPatch(typeof(ViewManager))]
-		public class ViewManagerPatches
-		{
-			[HarmonyPostfix, HarmonyPatch(nameof(ViewManager.SwitchToView))]
-			public static void Postfix(View view, bool immediate = false, bool lockAfter = false)
-			{
-				GrimoraPlugin.Log.LogDebug($"[ViewManager.SwitchToView] Called with view [{view}]");
+				return false;
 			}
 		}
 	}
