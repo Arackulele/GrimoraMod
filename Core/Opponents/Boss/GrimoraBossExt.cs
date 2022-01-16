b7cd3b789047310d7314002154d19f4556d99722
julian-perge <julianperge@gmail.com>
2022-01-15 20:21:28 -0500

    Added more logic for GrimoraBossExt and GrimoraBossSequencer

--- Core/Opponents/Boss/GrimoraBossExt.cs
+++ Core/Opponents/Boss/GrimoraBossExt.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using DiskCardGame;
 using UnityEngine;
 
@@ -7,14 +6,34 @@ namespace GrimoraMod
 {
 	public class GrimoraBossExt : BaseBossExt
 	{
-		
 		public override StoryEvent EventForDefeat => StoryEvent.PhotoDroneSeenInCabin;
-		
+
 		public override Type Opponent => (Type)1004;
-		
+
 		public override string DefeatedPlayerDialogue => "Thank you!";
 
 		public override int StartingLives => 3;
+
+
+		public override IEnumerator IntroSequence(EncounterData encounter)
+		{
+			yield return new WaitForSeconds(0.5f);
+			// InitializeAudioSources();
+			yield return new WaitForSeconds(0.25f);
+			AudioController.Instance.SetLoopVolume(1f, 0.5f);
+			yield return new WaitForSeconds(1f);
+			yield return Singleton<TextDisplayer>.Instance.PlayDialogueEvent("LeshyBossIntro1",
+				TextDisplayer.MessageAdvanceMode.Input);
+			yield return new WaitForSeconds(0.75f);
+			yield return base.IntroSequence(encounter);
+			yield return new WaitForSeconds(0.25f);
+			yield return Singleton<TextDisplayer>.Instance.PlayDialogueEvent("LeshyBossAddCandle",
+				TextDisplayer.MessageAdvanceMode.Input);
+			yield return new WaitForSeconds(0.4f);
+			bossSkull.EnterHand();
+			yield return new WaitForSeconds(3.5f);
+			Singleton<ViewManager>.Instance.SwitchToView(View.Default, immediate: false, lockAfter: true);
+		}
 
 		public override EncounterBlueprintData BuildInitialBlueprint()
 		{
@@ -40,13 +59,32 @@ namespace GrimoraMod
 				}
 				case 2:
 				{
-					foreach (var oppSlots in BoardManager.Instance.OpponentSlotsCopy)
+					yield return base.ClearBoard();
+
+					var oppSlots = CardSlotUtils.GetOpponentSlotsWithCards();
+
+					TextDisplayer.Instance.ShowUntilInput("LET THE BONE LORD COMMETH!",
+						letterAnimation: TextDisplayer.LetterAnimation.WavyJitter);
+
+					yield return BoardManager.Instance.CreateCardInSlot(
+						CardLoader.GetCardByName(GrimoraPlugin.NameBonelord), oppSlots[2], 0.2f
+					);
+					yield return new WaitForSeconds(0.25f);
+
+					oppSlots.RemoveAt(2);
+
+					TextDisplayer.Instance.ShowUntilInput("RISE MY ARMY! RIIIIIIIIIISE!",
+						letterAnimation: TextDisplayer.LetterAnimation.WavyJitter);
+
+					foreach (CardSlot cardSlot in oppSlots)
 					{
 						yield return BoardManager.Instance.CreateCardInSlot(
-							CardLoader.GetCardByName("ara_Bonelord"), oppSlots, 0.2f
+							CardLoader.GetCardByName(GrimoraPlugin.NameSkeletonArmy), cardSlot, 0.2f
 						);
+
 						yield return new WaitForSeconds(0.25f);
 					}
+
 
 					break;
 				}
