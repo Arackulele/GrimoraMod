aafbfbc51f9f9c21bb391dac0de1bfcbe8d05942
julian-perge <julianperge@gmail.com>
2022-01-16 09:45:22 -0500

    Slight refactor for SetActiveNode

--- Core/MapNodeManagerPatches.cs
+++ Core/MapNodeManagerPatches.cs
@@ -7,24 +7,18 @@ namespace GrimoraMod
 	public class MapNodeManagerPatches
 	{
 		[HarmonyPrefix, HarmonyPatch(nameof(MapNodeManager.SetActiveNode))]
-		public static bool PrefixChangeActiveNodeLogic(ref MapNode newActiveNode)
+		public static bool PrefixChangeActiveNodeLogic(ref MapNodeManager __instance, ref MapNode newActiveNode)
 		{
 			if (!SaveManager.SaveFile.IsGrimora)
 			{
 				return true;
 			}
 
-			if (newActiveNode is null)
-			{
-				GrimoraPlugin.Log.LogDebug($"[SetActiveNode] NewActiveNode is null, setting to current active node");
-				newActiveNode = MapNodeManager.Instance.ActiveNode;
-				newActiveNode.nodeId = RunState.Run.currentNodeId;
-			}
-
-			GrimoraPlugin.Log.LogDebug(
-				$"[SetActiveNode] Setting activeNode active x{newActiveNode.Data.gridX}y{newActiveNode.Data.gridY}");
+			// GrimoraPlugin.Log.LogDebug($"[SetActiveNode] activeNode x{newActiveNode.Data.gridX}y{newActiveNode.Data.gridY}");
 			newActiveNode.SetActive(true);
 			ChessboardNavGrid.instance.SetPlayerAdjacentNodesActive();
+
+			__instance.ActiveNode = newActiveNode;
 
 			return false;
 		}
