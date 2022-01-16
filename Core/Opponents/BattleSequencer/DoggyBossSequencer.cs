687c79da98703ee9796b651a3974df29250b42f1
julian-perge <julianperge@gmail.com>
2022-01-15 20:20:56 -0500

    General class cleanup.

--- Core/Opponents/BattleSequencer/DoggyBossSequencer.cs
+++ Core/Opponents/BattleSequencer/DoggyBossSequencer.cs
@@ -5,9 +5,17 @@ namespace GrimoraMod
 {
 	public class DoggyBossSequencer : Part1BossBattleSequencer
 	{
-		public override Opponent.Type BossType => Opponent.Type.ProspectorBoss;
+		public override Opponent.Type BossType => BaseBossExt.DoggyOpponent;
 
 		public override StoryEvent DefeatedStoryEvent => StoryEvent.TutorialRunCompleted;
+
+		public override EncounterData BuildCustomEncounter(CardBattleNodeData nodeData)
+		{
+			return new EncounterData()
+			{
+				opponentType = BossType
+			};
+		}
 
 		public override bool RespondsToOtherCardDie(
 			PlayableCard card, CardSlot deathSlot, bool fromCombat, PlayableCard killer
@@ -27,20 +35,10 @@ namespace GrimoraMod
 			}
 		}
 
-
-		public override EncounterData BuildCustomEncounter(CardBattleNodeData nodeData)
-		{
-			return new EncounterData()
-			{
-				opponentType = BaseBossExt.DoggyOpponent
-			};
-		}
-
 		public override bool RespondsToUpkeep(bool playerUpkeep)
 		{
 			return playerUpkeep;
 		}
-
 
 		public override IEnumerator OnUpkeep(bool playerUpkeep)
 		{
