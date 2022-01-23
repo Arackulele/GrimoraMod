6bde48caf1a7a862546787537dc4c0648263dbe0
julian-perge <julianperge@gmail.com>
2022-01-23 09:34:46 -0500

    Corrected updated method call for AddPieceToRemovedPiecesConfig

--- Patches/Chessboard/Pieces/ChestPatches.cs
+++ Patches/Chessboard/Pieces/ChestPatches.cs
@@ -9,7 +9,7 @@ namespace GrimoraMod;
 public class ChestPatches
 {
 	[HarmonyPrefix, HarmonyPatch(nameof(ChessboardChestPiece.Start))]
-	public static bool StartPrefix(ref ChessboardChestPiece __instance)
+	public static void StartPrefix(ref ChessboardChestPiece __instance)
 	{
 		// this position code exists only in ChessboardEnemyPiece, which is why we need a patch for it
 		__instance.gameObject.transform.position
@@ -18,27 +18,23 @@ public class ChestPatches
 				.zones[__instance.gridXPos, __instance.gridYPos]
 				.transform
 				.position;
-
-		ChessboardNavGrid.instance
-			.zones[__instance.gridXPos, __instance.gridYPos]
-			.GetComponent<ChessboardMapNode>()
-			.OccupyingPiece = __instance;
-
-		// if we place chests with a different type of node data during creation, the Start method will overwrite it 
-		__instance.NodeData ??= new CardChoicesNodeData();
-
-		return false;
 	}
 
 	[HarmonyPostfix, HarmonyPatch(nameof(ChessboardChestPiece.OpenSequence))]
 	public static IEnumerator OpenSequencePostfix(IEnumerator enumerator, ChessboardChestPiece __instance)
 	{
+		if (!SaveManager.SaveFile.IsGrimora)
+		{
+			yield return enumerator;
+			yield break;
+		}
+
 		GrimoraPlugin.Log.LogDebug($"[ChessboardChestPiece.OpenSequence] Piece [{__instance.name}]");
-		ConfigHelper.Instance.AddPieceToRemovedPiecesConfig(__instance.name);
+		ConfigHelper.AddPieceToRemovedPiecesConfig(__instance.name);
 
 		MapNodeManager.Instance.SetAllNodesInteractable(false);
 
-		ViewManager.Instance.SetViewLocked();
+		ViewManager.Instance.Controller.LockState = ViewLockState.Locked;
 
 		PlayerMarker.Instance.Anim.Play("knock against", 0, 0f);
 		yield return new WaitForSeconds(0.05f);
@@ -46,7 +42,7 @@ public class ChestPatches
 		__instance.anim.Play("open", 0, 0f);
 		yield return new WaitForSeconds(0.25f);
 
-		ViewManager.Instance.SetViewUnlocked();
+		ViewManager.Instance.Controller.LockState = ViewLockState.Unlocked;
 
 		GameFlowManager.Instance.TransitionToGameState(GameState.SpecialCardSequence, __instance.NodeData);
 	}
