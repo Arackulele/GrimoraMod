9f2eb6ce27a30aad9fe8e28489fb06b6b5e1fe0c
julian-perge <julianperge@gmail.com>
2022-01-27 18:53:04 -0500

    Added initial SkinCrawler commit. Still needs fleshed out.

---
+++ Abilities/SkinCrawler.cs
@@ -0,0 +1,20 @@
+﻿using APIPlugin;
+using DiskCardGame;
+
+namespace GrimoraMod;
+
+public class SkinCrawler : AbilityBehaviour
+{
+	public static Ability ability;
+	public override Ability Ability => ability;
+
+
+	public static NewAbility Create()
+	{
+		const string rulebookDescription =
+			"At the end of the owner's turn, [creature] will move in the direction inscribed in the sigil and, if possible," +
+			" hide under the card providing a +1 buff";
+
+		return ApiUtils.CreateAbility<SkinCrawler>(rulebookDescription);
+	}
+}
