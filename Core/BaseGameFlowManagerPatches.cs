de14b2225b582eb9ab051dfb5ddca68f6dafa86c
julian-perge <julianperge@gmail.com>
2022-01-28 21:05:19 -0500

    Added more null checks

--- Core/BaseGameFlowManagerPatches.cs
+++ Core/BaseGameFlowManagerPatches.cs
@@ -89,7 +89,8 @@ public class BaseGameFlowManagerPatches
 	private static void AddHammer()
 	{
 		Log.LogDebug($"Creating hammer");
-		HammerItemSlot = UnityEngine.Object.Instantiate(
+
+		HammerItemSlot ??= UnityEngine.Object.Instantiate(
 			ResourceBank.Get<Part3ItemsManager>("Prefabs/Items/ItemsManager_Part3"),
 			new Vector3(-2.69f, 5.82f, -0.48f),
 			Quaternion.Euler(270f, 315f, 0f),
@@ -101,42 +102,45 @@ public class BaseGameFlowManagerPatches
 	{
 		BoardManager3D boardManager = UnityEngine.Object.FindObjectOfType<BoardManager3D>();
 
-		ResourceEnergy = UnityEngine.Object.Instantiate(
-			ResourceBank.Get<ResourceDrone>("Prefabs/CardBattle/ResourceModules"),
-			new Vector3(5.3f, 5.5f, 1.92f),
-			Quaternion.Euler(270f, 0f, -146.804f),
-			boardManager.transform
-		);
-
-		Color grimoraTextColor = new Color(0.420f, 1f, 0.63f);
-		ResourceEnergy.name = "Grimora Resource Modules";
-		ResourceEnergy.baseCellColor = grimoraTextColor;
-		ResourceEnergy.highlightedCellColor = new Color(1, 1, 0.23f);
-
-		// Log.LogDebug($"[AddEnergyDrone] Disabling animation");
-		Animator animator = ResourceEnergy.GetComponentInChildren<Animator>();
-		animator.enabled = false;
-
-		Transform moduleEnergy = animator.transform.GetChild(0);
-		// Log.LogDebug($"[AddEnergyDrone] Getting module energy and setting mesh to null");
-		moduleEnergy.gameObject.GetComponent<MeshFilter>().mesh = null;
-
-		int modulesChildren = moduleEnergy.childCount;
-		for (int i = 1; i < 7; i++)
+		if (boardManager is not null && ResourceEnergy is null)
 		{
-			Transform energyCell = moduleEnergy.GetChild(i);
-			// Log.LogDebug($"[AddEnergyDrone] Energy cell [{energyCell.name}]");
-			energyCell.gameObject.GetComponent<MeshFilter>().mesh = null;
-			var energyCellCase = energyCell.GetChild(0);
-			energyCellCase.GetChild(0).GetComponent<MeshRenderer>().material.SetColor(EmissionColor, grimoraTextColor);
-			energyCellCase.GetChild(1).GetComponent<MeshFilter>().mesh = null;
-			energyCellCase.GetChild(2).GetComponent<MeshFilter>().mesh = null;
-		}
+			ResourceEnergy = UnityEngine.Object.Instantiate(
+				ResourceBank.Get<ResourceDrone>("Prefabs/CardBattle/ResourceModules"),
+				new Vector3(5.3f, 5.5f, 1.92f),
+				Quaternion.Euler(270f, 0f, -146.804f),
+				boardManager.transform
+			);
 
-		// Log.LogDebug($"[AddEnergyDrone] Setting Connector inactive");
-		moduleEnergy.Find("Connector").gameObject.SetActive(false);
-		// Log.LogDebug($"[AddEnergyDrone] Setting Propellers inactive");
-		moduleEnergy.Find("Propellers").gameObject.SetActive(false);
+			Color grimoraTextColor = new Color(0.420f, 1f, 0.63f);
+			ResourceEnergy.name = "Grimora Resource Modules";
+			ResourceEnergy.baseCellColor = grimoraTextColor;
+			ResourceEnergy.highlightedCellColor = new Color(1, 1, 0.23f);
+
+			// Log.LogDebug($"[AddEnergyDrone] Disabling animation");
+			Animator animator = ResourceEnergy.GetComponentInChildren<Animator>();
+			animator.enabled = false;
+
+			Transform moduleEnergy = animator.transform.GetChild(0);
+			// Log.LogDebug($"[AddEnergyDrone] Getting module energy and setting mesh to null");
+			moduleEnergy.gameObject.GetComponent<MeshFilter>().mesh = null;
+
+			int modulesChildren = moduleEnergy.childCount;
+			for (int i = 1; i < 7; i++)
+			{
+				Transform energyCell = moduleEnergy.GetChild(i);
+				// Log.LogDebug($"[AddEnergyDrone] Energy cell [{energyCell.name}]");
+				energyCell.gameObject.GetComponent<MeshFilter>().mesh = null;
+				var energyCellCase = energyCell.GetChild(0);
+				energyCellCase.GetChild(0).GetComponent<MeshRenderer>().material.SetColor(EmissionColor, grimoraTextColor);
+				energyCellCase.GetChild(1).GetComponent<MeshFilter>().mesh = null;
+				energyCellCase.GetChild(2).GetComponent<MeshFilter>().mesh = null;
+			}
+
+			// Log.LogDebug($"[AddEnergyDrone] Setting Connector inactive");
+			moduleEnergy.Find("Connector").gameObject.SetActive(false);
+			// Log.LogDebug($"[AddEnergyDrone] Setting Propellers inactive");
+			moduleEnergy.Find("Propellers").gameObject.SetActive(false);
+		}
 	}
 
 	private static void ChangeStartDeckIfNotAlreadyChanged()
@@ -155,28 +159,34 @@ public class BaseGameFlowManagerPatches
 	{
 		// Log.LogDebug($"[ChangeChessboardToExtendedClass] Adding MapExt to ChessboardMapGameObject");
 		GameObject boardObj = GameObject.Find("ChessboardGameMap");
-		ChessboardMap boardComp = boardObj.GetComponent<ChessboardMap>();
+		ChessboardMapExt ext = boardObj.gameObject.GetComponent<ChessboardMapExt>();
 
-		ChessboardMapExt ext = boardObj.gameObject.AddComponent<ChessboardMapExt>();
-		ext.dynamicElementsParent = boardComp.dynamicElementsParent;
-		ext.mapAnim = boardComp.mapAnim;
-		ext.navGrid = boardComp.navGrid;
-		ext.pieces = new List<ChessboardPiece>();
-		ext.defaultPosition = boardComp.defaultPosition;
-
-		// GrimoraPlugin.Log.LogDebug($"Destroying old chessboard component");
-		UnityEngine.Object.Destroy(boardComp);
-
-		var initialStartingPieces = UnityEngine.Object.FindObjectsOfType<ChessboardPiece>();
-		// Log.LogDebug($"[ChangeChessboardToExtendedClass] Resetting initial pieces" +
-		//              $" {string.Join(", ", initialStartingPieces.Select(_ => _.name))}");
-
-		foreach (var piece in initialStartingPieces)
+		if (ext is null)
 		{
-			ext.pieces.Remove(piece);
-			piece.MapNode.OccupyingPiece = null;
-			piece.gameObject.SetActive(false);
-			UnityEngine.Object.Destroy(piece.gameObject);
+			ChessboardMap boardComp = boardObj.GetComponent<ChessboardMap>();
+
+			ext = boardObj.gameObject.AddComponent<ChessboardMapExt>();
+
+			ext.dynamicElementsParent = boardComp.dynamicElementsParent;
+			ext.mapAnim = boardComp.mapAnim;
+			ext.navGrid = boardComp.navGrid;
+			ext.pieces = new List<ChessboardPiece>();
+			ext.defaultPosition = boardComp.defaultPosition;
+
+			// GrimoraPlugin.Log.LogDebug($"Destroying old chessboard component");
+			UnityEngine.Object.Destroy(boardComp);
+
+			var initialStartingPieces = UnityEngine.Object.FindObjectsOfType<ChessboardPiece>();
+			// Log.LogDebug($"[ChangeChessboardToExtendedClass] Resetting initial pieces" +
+			//              $" {string.Join(", ", initialStartingPieces.Select(_ => _.name))}");
+
+			foreach (var piece in initialStartingPieces)
+			{
+				ext.pieces.Remove(piece);
+				piece.MapNode.OccupyingPiece = null;
+				piece.gameObject.SetActive(false);
+				UnityEngine.Object.Destroy(piece.gameObject);
+			}
 		}
 	}
 
@@ -198,21 +208,24 @@ public class BaseGameFlowManagerPatches
 
 		// GrimoraPlugin.Log.LogDebug($"Creating RareCardChoiceSelector");
 
-		GameObject rareCardChoicesSelector = UnityEngine.Object.Instantiate(
-			ResourceBank.Get<GameObject>("Prefabs/SpecialNodeSequences/RareCardChoiceSelector"),
-			specialNodeHandler.transform
-		);
+		if (specialNodeHandler is not null)
+		{
+			GameObject rareCardChoicesSelector = UnityEngine.Object.Instantiate(
+				ResourceBank.Get<GameObject>("Prefabs/SpecialNodeSequences/RareCardChoiceSelector"),
+				specialNodeHandler.transform
+			);
 
-		RareCardChoicesSequencer sequencer = rareCardChoicesSelector.GetComponent<RareCardChoicesSequencer>();
+			RareCardChoicesSequencer sequencer = rareCardChoicesSelector.GetComponent<RareCardChoicesSequencer>();
 
-		// GrimoraPlugin.Log.LogDebug($"-> Setting RareCardChoicesSequencer choice generator to Part1RareChoiceGenerator");
-		sequencer.choiceGenerator = rareCardChoicesSelector.AddComponent<GrimoraRareChoiceGenerator>();
+			// GrimoraPlugin.Log.LogDebug($"-> Setting RareCardChoicesSequencer choice generator to Part1RareChoiceGenerator");
+			sequencer.choiceGenerator = rareCardChoicesSelector.AddComponent<GrimoraRareChoiceGenerator>();
 
-		// GrimoraPlugin.Log.LogDebug($"-> Setting RareCardChoicesSequencer selectableCardPrefab to SelectableCard_Grimora");
-		sequencer.selectableCardPrefab = PrefabGrimoraSelectableCard;
+			// GrimoraPlugin.Log.LogDebug($"-> Setting RareCardChoicesSequencer selectableCardPrefab to SelectableCard_Grimora");
+			sequencer.selectableCardPrefab = PrefabGrimoraSelectableCard;
 
-		// GrimoraPlugin.Log.LogDebug($"-> Setting SpecialNodeHandler rareCardChoiceSequencer to sequencer");
-		specialNodeHandler.rareCardChoiceSequencer = sequencer;
+			// GrimoraPlugin.Log.LogDebug($"-> Setting SpecialNodeHandler rareCardChoiceSequencer to sequencer");
+			specialNodeHandler.rareCardChoiceSequencer = sequencer;
+		}
 	}
 
 
