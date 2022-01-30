c1a0230e90690cbdfd8ea1a3098e2f6bb2bb6012
julian-perge <julianperge@gmail.com>
2022-01-29 23:17:14 -0500

    Cleaned up and added logic to Skin Crawler to now look at both adjacent slots.

--- Abilities/SkinCrawler.cs
+++ Abilities/SkinCrawler.cs
@@ -12,9 +12,30 @@ namespace GrimoraMod;
 public class SkinCrawler : AbilityBehaviour
 {
 	public static Ability ability;
+
+	public static bool PlayedDialogue = false;
 	public override Ability Ability => ability;
 
 	private PlayableCard hidingUnderCard;
+
+	public static SkinCrawler GetSkinCrawlerFromSlot(CardSlot slot)
+	{
+		if (slot.Card is not null)
+		{
+			PlayableCard card = slot.Card;
+			for (int i = 0; i < card.transform.childCount; i++)
+			{
+				SkinCrawler skinCrawler = card.transform.GetChild(i).GetComponent<SkinCrawler>();
+				if (skinCrawler is not null)
+				{
+					// now we get the card itself to add to the list
+					return skinCrawler;
+				}
+			}
+		}
+
+		return null;
+	}
 
 	public override bool RespondsToResolveOnBoard()
 	{
@@ -25,12 +46,23 @@ public class SkinCrawler : AbilityBehaviour
 	{
 		// CardSlot toLeft = BoardManager.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: true);
 		// Log.LogDebug($"[SkinCrawler] Checking if adj slots from [{base.Card.Slot}] are not null");
+		CardSlot slotToPick = null;
 		CardSlot toLeftSlot = BoardManager.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: true);
 		CardSlot toRightSlot = BoardManager.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: false);
-		if (toRightSlot is not null && toRightSlot.Card is not null)
+
+		if (toLeftSlot is not null && toLeftSlot.Card is not null)
 		{
-			Transform toRightTransform = toRightSlot.Card.transform;
-			Transform toRightSlotTransform = toRightSlot.Card.Slot.transform;
+			slotToPick = toLeftSlot;
+		}
+		else if (toRightSlot is not null && toRightSlot.Card is not null)
+		{
+			slotToPick = toRightSlot;
+		}
+
+		if (slotToPick is not null && GetSkinCrawlerFromSlot(slotToPick) is null)
+		{
+			PlayableCard cardToPick = slotToPick.Card;
+			CardSlot cardSlotToPick = slotToPick.Card.Slot;
 
 			ViewManager.Instance.SwitchToView(View.Board);
 
@@ -38,9 +70,9 @@ public class SkinCrawler : AbilityBehaviour
 			// to the left and up, like something is being added under it
 			Vector3 vector = new Vector3(0f, 0.25f, 0f);
 			// do to card that will be hiding Boo Hag
-			Tween.Position(toRightTransform, toRightSlotTransform.position + vector, 0.1f, 0f, Tween.EaseInOut);
+			Tween.Position(cardToPick.transform, cardSlotToPick.transform.position + vector, 0.1f, 0f, Tween.EaseInOut);
 
-			Vector3 cardRot = toRightSlot.Card.transform.rotation.eulerAngles;
+			Vector3 cardRot = cardToPick.transform.rotation.eulerAngles;
 
 			// rotate on z-axis, as if you rotated your hand holding the card counter-clockwise
 			// Tween.Rotate(toRightTransform, new Vector3(0f, 0f, 0f), Space.World, 0.1f, 0f, Tween.EaseInOut);
@@ -56,7 +88,7 @@ public class SkinCrawler : AbilityBehaviour
 			// Log.LogDebug($"[SkinCrawler] moving BooHag to [{toRightSlotTransform.position}]");
 			TweenBase tween = Tween.Position(
 				base.Card.transform,
-				toRightSlot.Card.Slot.transform.position + new Vector3(0f, 0f, 0.31f),
+				cardSlotToPick.transform.position + new Vector3(0f, 0f, 0.31f),
 				0.4f,
 				0f,
 				Tween.EaseOut
@@ -65,7 +97,7 @@ public class SkinCrawler : AbilityBehaviour
 			while (tween.Status is not Tween.TweenStatus.Finished)
 			{
 				// Log.LogDebug($"[SkinCrawler] playing negation effect in loop");
-				toRightSlot.Card.Anim.StrongNegationEffect();
+				cardToPick.Anim.StrongNegationEffect();
 				yield return new WaitForSeconds(0.1f);
 			}
 
@@ -73,33 +105,41 @@ public class SkinCrawler : AbilityBehaviour
 
 			// rotate base card with it's original rotation values so that it lays flat on the board again
 			// Log.LogDebug($"[SkinCrawler] rotating [{toRightSlot.Card.Info.name}]");
-			Tween.Rotation(toRightTransform, cardRot, 0.1f, 0f, Tween.EaseInOut);
+			Tween.Rotation(cardToPick.transform, cardRot, 0.1f, 0f, Tween.EaseInOut);
 
 			// offset the card to be a little higher
 			// Log.LogDebug($"[SkinCrawler] Setting height offset");
-			toRightSlot.Card.SlotHeightOffset = 0.13f;
+			cardToPick.SlotHeightOffset = 0.13f;
 
 			// reassign the card to the slot
-			Log.LogDebug($"[SkinCrawler] Assigning [{toRightSlot.Card.Info.name}] to slot");
-			yield return BoardManager.Instance.AssignCardToSlot(toRightSlot.Card, toRightSlot.Card.Slot);
+			Log.LogDebug($"[SkinCrawler] Assigning [{cardToPick.Info.name}] to slot");
+			yield return BoardManager.Instance.AssignCardToSlot(cardToPick, cardSlotToPick);
 
-			toRightSlot.Card.Anim.Anim.StartPlayback();
-			yield return new WaitForSeconds(0.25f);
 			// Log.LogDebug($"[SkinCrawler] Adding temporary mod");
-			toRightSlot.Card.AddTemporaryMod(new CardModificationInfo() { attackAdjustment = 1, healthAdjustment = 1 });
+			cardToPick.AddTemporaryMod(new CardModificationInfo() { attackAdjustment = 1, healthAdjustment = 1 });
 
 			yield return new WaitUntil(() =>
-				!Tween.activeTweens.Exists((TweenBase t) => t.targetInstanceID == toRightSlot.Card.transform.GetInstanceID())
+				!Tween.activeTweens.Exists((TweenBase t) => t.targetInstanceID == cardToPick.transform.GetInstanceID())
 			);
 
 			Log.LogDebug($"[SkinCrawler] Setting Boo Hag as child of card");
-			base.transform.SetParent(toRightSlot.Card.transform); // now a child of the playable card
+			base.transform.SetParent(cardToPick.transform); // now a child of the playable card
 			Log.LogDebug($"[SkinCrawler] Setting Boo Hag slot [{base.Card.Slot.Index}] to null");
 			BoardManager.Instance.playerSlots[base.Card.Slot.Index].Card = null;
-			Log.LogDebug($"[SkinCrawler] Setting Boo Hag slot to [{toRightSlot.Card.Slot}]");
-			base.Card.Slot = toRightSlot.Card.Slot;
+			Log.LogDebug($"[SkinCrawler] Setting Boo Hag slot to [{cardSlotToPick}]");
+			base.Card.Slot = cardSlotToPick;
 
-			hidingUnderCard = toRightSlot.Card;
+			hidingUnderCard = cardToPick;
+		}
+		else
+		{
+			if (!PlayedDialogue)
+			{
+				PlayedDialogue = true;
+				ViewManager.Instance.SwitchToView(View.Board);
+				yield return TextDisplayer.Instance.ShowUntilInput("Poor thing couldn't find a host");
+				yield return base.Card.Die(false);
+			}
 		}
 	}
 
@@ -143,16 +183,14 @@ public class SkinCrawlerPatches
 		List<PlayableCard> newList = new List<PlayableCard>(__result);
 		foreach (var card in __result)
 		{
-			for (int i = 0; i < card.transform.childCount; i++)
+			SkinCrawler skinCrawler = SkinCrawler.GetSkinCrawlerFromSlot(card.Slot);
+			if (skinCrawler is not null)
 			{
-				SkinCrawler skinCrawler = card.transform.GetChild(i).GetComponent<SkinCrawler>();
-				if (skinCrawler is not null)
-				{
-					// now we get the card itself to add to the list
-					newList.Add(skinCrawler.GetComponent<PlayableCard>());
-				}
+				// now we get the card itself to add to the list
+				newList.Add(skinCrawler.GetComponent<PlayableCard>());
 			}
 		}
+
 		__result = newList;
 	}
 }
