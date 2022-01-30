866e351d01b0dbeb84c4abb62efbf67550359e6e
julian-perge <julianperge@gmail.com>
2022-01-30 00:04:59 -0500

    Set Screaming Skull cost to 6 bones from 8

--- Cards/ScreamingSkull.cs
+++ Cards/ScreamingSkull.cs
@@ -18,7 +18,7 @@ public partial class GrimoraPlugin
 			.SetAsNormalCard()
 			.SetAbilities(AreaOfEffectStrike.ability)
 			.SetBaseAttackAndHealth(1, 1)
-			.SetBoneCost(8)
+			.SetBoneCost(6)
 			// .SetDescription("Going into that well wasn't the best idea...")
 			.SetNames(NameScreamingSkull, "Screaming Skull")
 			.Build()
