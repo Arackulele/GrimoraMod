c70602b7c835feb403c7764014e050c62fe9f5c5
julian-perge <julianperge@gmail.com>
2022-01-31 09:25:41 -0500

    Reduce if nesting, and added extra logic to now set MapNodeManager.Instance.ActiveNode if it is null

--- Core/Chessboard/PlayerMarkerPatches.cs
+++ Core/Chessboard/PlayerMarkerPatches.cs
@@ -9,13 +9,25 @@ public class PlayerMarkerPatches
 	[HarmonyPostfix, HarmonyPatch(nameof(AnimatedGameMapMarker.Show))]
 	public static void PostfixAddExtraLogicAfterUnrolling(AnimatedGameMapMarker __instance)
 	{
-		if (SaveManager.SaveFile.IsGrimora && __instance is PlayerMarker)
+		if (!SaveManager.SaveFile.IsGrimora || __instance is not PlayerMarker)
 		{
-			// Have you call the setter for this twice otherwise the PlayerMarker might appear at the previous active node visually,
-			//	but is actually at the new node.
-			// e.g. Shows the marker at 0,1 , but is actually at 6,7
-			GrimoraPlugin.Log.LogDebug($"[AnimatedGameMapMarker.Show] Setting PlayerMarker");
-			PlayerMarker.Instance.transform.position = MapNodeManager.Instance.ActiveNode.transform.position;
+			return;
 		}
+
+		// Have you call the setter for this twice otherwise the PlayerMarker might appear at the previous active node visually,
+		//	but is actually at the new node.
+		// e.g. Shows the marker at 0,1 , but is actually at 6,7
+		if (MapNodeManager.Instance.ActiveNode is null)
+		{
+			GrimoraPlugin.Log.LogDebug($"[AnimatedGameMapMarker.Show] activeNode is null, setting");
+			MapNodeManager.Instance.ActiveNode
+				= ChessboardNavGrid
+					.instance
+					.zones[GrimoraSaveData.Data.gridX, GrimoraSaveData.Data.gridY]
+					.GetComponent<ChessboardMapNode>();
+		}
+
+		GrimoraPlugin.Log.LogDebug($"[AnimatedGameMapMarker.Show] Setting PlayerMarker");
+		PlayerMarker.Instance.transform.position = MapNodeManager.Instance.ActiveNode.transform.position;
 	}
 }
