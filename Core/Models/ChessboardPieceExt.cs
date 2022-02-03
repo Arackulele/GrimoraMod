394dfb4337ad68f1957b821bc56d466e00c03705
julian-perge <julianperge@gmail.com>
2022-02-03 02:59:47 -0500

    Created ChessboardPieceExt for custom made piece types.
    Added Boss, Boneyard, CardRemove, ElectricChair, and GoatEye custom piece types.

---
+++ Core/Models/ChessboardPieceExt.cs
@@ -0,0 +1,28 @@
+﻿using DiskCardGame;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+public class ChessboardPieceExt : ChessboardPiece
+{
+
+	public float newYPosition;
+	public float newScale;
+	
+	private void Start()
+	{
+		base.transform.position = ChessboardNavGrid.instance.zones[gridXPos, gridYPos].transform.position;
+		if (newYPosition != 0f)
+		{
+			Vector3 copy = base.transform.localPosition;
+			base.transform.localPosition = new Vector3(copy.x, newYPosition, copy.z);
+		}
+
+		if (newScale != 0f)
+		{
+			base.transform.localScale = new Vector3(newScale, newScale, newScale);
+		}
+		ChessboardNavGrid.instance.zones[gridXPos, gridYPos].GetComponent<ChessboardMapNode>().OccupyingPiece = this;
+		base.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+	}
+}
