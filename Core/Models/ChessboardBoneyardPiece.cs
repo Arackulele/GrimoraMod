63ff37f3f5d1626f922876fbe5849dc602748af3
julian-perge <julianperge@gmail.com>
2022-02-03 09:33:09 -0500

    Added comments for what the piece is to do

--- Core/Models/ChessboardBoneyardPiece.cs
+++ Core/Models/ChessboardBoneyardPiece.cs
@@ -4,6 +4,7 @@ namespace GrimoraMod;
 
 public class ChessboardBoneyardPiece : ChessboardPieceExt
 {
+	// Bury one of your cards to give it Brittle and halve its cost
 	public ChessboardBoneyardPiece()
 	{
 		newScale = 1.25f;
--- Core/Models/ChessboardElectricChairPiece.cs
+++ Core/Models/ChessboardElectricChairPiece.cs
@@ -1,9 +1,8 @@
-﻿using DiskCardGame;
-
-namespace GrimoraMod;
+﻿namespace GrimoraMod;
 
 public class ChessboardElectricChairPiece : ChessboardPieceExt
 {
+	// Give a random positive sigil to your Card, similar to campfire, has an increasing chance the card will die
 	public ChessboardElectricChairPiece()
 	{
 		newYPosition = 1.2f;
