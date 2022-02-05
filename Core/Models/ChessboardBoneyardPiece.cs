0ec29964d307c36dafda0231781432a123beafe7
julian-perge <julianperge@gmail.com>
2022-02-05 07:34:19 -0500

    Added CardStatBoostNodeData to Boneyard piece

--- Core/Models/ChessboardBoneyardPiece.cs
+++ Core/Models/ChessboardBoneyardPiece.cs
@@ -7,6 +7,8 @@ public class ChessboardBoneyardPiece : ChessboardPieceExt
 	// Bury one of your cards to give it Brittle and halve its cost
 	public ChessboardBoneyardPiece()
 	{
+		NodeData = new CardStatBoostNodeData();
 		newScale = 1.25f;
 	}
+	
 }
