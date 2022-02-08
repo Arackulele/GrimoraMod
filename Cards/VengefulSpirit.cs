f1a3b3ff01c5606ffb2e0fe628617cf4e913a42d
julian-perge <julianperge@gmail.com>
2022-02-08 06:20:32 -0500

    Corrected typo in Vengeful Spirit.
    Changed Skelemancer to VengefulSpirit.
    
    Added Skelemagus and Revenant emissions.

--- Cards/VengefulSpirit.cs
+++ Cards/VengefulSpirit.cs
@@ -4,16 +4,16 @@ namespace GrimoraMod;
 
 public partial class GrimoraPlugin
 {
-	public const string NameSkelemancer = "GrimoraMod_Skelemancer";
+	public const string NameVengefulSpirit = "GrimoraMod_VengefulSpirit";
 
-	private void Add_Skelemancer()
+	private void Add_VengefulSpirit()
 	{
 		NewCard.Add(CardBuilder.Builder
 			.SetAsNormalCard()
 			.SetBaseAttackAndHealth(1, 1)
 			.SetEnergyCost(3)
-			.SetDescription("Coming for Vengence")
-			.SetNames(NameSkelemancer, "Vangeful Spirit")
+			.SetDescription("Coming for vengeance!")
+			.SetNames(NameVengefulSpirit, "Vengeful Spirit")
 			.Build()
 		);
 	}
