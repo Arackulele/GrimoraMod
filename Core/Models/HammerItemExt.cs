a8abafa4705810c22d80fba6a4e18f2a5dd56335
julian-perge <julianperge@gmail.com>
2022-02-13 07:32:45 -0500

    Refactored HammerItemExt to now also call GetValidTargets. If this is not there, the game thinks the hammer is usable.

--- Core/Models/HammerItemExt.cs
+++ Core/Models/HammerItemExt.cs
@@ -9,31 +9,37 @@ public class HammerItemExt : HammerItem
 {
 	internal int UseCounter = 0;
 	private bool _dialoguePlayed = false;
-	
+	private static readonly int Hit = Animator.StringToHash("hit");
+
 	public override IEnumerator OnValidTargetSelected(CardSlot targetSlot, GameObject firstPersonItem)
 	{
-		firstPersonItem.GetComponentInChildren<Animator>().SetTrigger("hit");
+		firstPersonItem.GetComponentInChildren<Animator>().SetTrigger(Hit);
 		yield return new WaitForSeconds(0.166f);
-		Tween.Position(firstPersonItem.transform, firstPersonItem.transform.position + Vector3.back * 4f, 0.15f, 0.2f, Tween.EaseOut, Tween.LoopType.None, null, delegate
-		{
-			firstPersonItem.gameObject.SetActive(value: false);
-		});
+		Tween.Position(firstPersonItem.transform,
+			firstPersonItem.transform.position + Vector3.back * 4f,
+			0.15f,
+			0.2f,
+			Tween.EaseOut,
+			Tween.LoopType.None,
+			null,
+			delegate { firstPersonItem.gameObject.SetActive(false); });
 		if (targetSlot.Card != null)
 		{
 			yield return targetSlot.Card.TakeDamage(100, null);
 		}
+
 		yield return new WaitForSeconds(0.65f);
 		UseCounter++;
 	}
-	
+
 	public override bool ExtraActivationPrerequisitesMet()
 	{
-		return UseCounter < 3;
+		return UseCounter < 3 && GetValidTargets().Count > 0;
 	}
 
 	public override void OnExtraActivationPrerequisitesNotMet()
 	{
-		if (!_dialoguePlayed)
+		if (!_dialoguePlayed && UseCounter >= 3)
 		{
 			StartCoroutine(
 				TextDisplayer.Instance.ShowUntilInput("The was the last one, I hope you used it well.")
