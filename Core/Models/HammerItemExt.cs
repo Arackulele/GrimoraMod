ebc98c52c1ed706fac08f88b3e20d97c339bc86c
julian-perge <julianperge@gmail.com>
2022-02-13 08:05:59 -0500

    Refactored logic for hammer to now check if opponent is kaycee and disallows breaking ice cubes.

--- Core/Models/HammerItemExt.cs
+++ Core/Models/HammerItemExt.cs
@@ -7,39 +7,45 @@ namespace GrimoraMod;
 
 public class HammerItemExt : HammerItem
 {
-	internal int UseCounter = 0;
-	private bool _dialoguePlayed = false;
 	private static readonly int Hit = Animator.StringToHash("hit");
+	private int _useCounter = 0;
+	private bool _dialoguePlayed = false;
 
 	public override IEnumerator OnValidTargetSelected(CardSlot targetSlot, GameObject firstPersonItem)
 	{
 		firstPersonItem.GetComponentInChildren<Animator>().SetTrigger(Hit);
 		yield return new WaitForSeconds(0.166f);
-		Tween.Position(firstPersonItem.transform,
+		Tween.Position(
+			firstPersonItem.transform,
 			firstPersonItem.transform.position + Vector3.back * 4f,
 			0.15f,
 			0.2f,
 			Tween.EaseOut,
 			Tween.LoopType.None,
 			null,
-			delegate { firstPersonItem.gameObject.SetActive(false); });
+			delegate { firstPersonItem.gameObject.SetActive(false); }
+		);
 		if (targetSlot.Card != null)
 		{
-			yield return targetSlot.Card.TakeDamage(100, null);
+			if (TurnManager.Instance.Opponent is not null
+			    && TurnManager.Instance.Opponent is KayceeBossOpponent
+			    && targetSlot.Card.HasAbility(Ability.IceCube)
+			    && _useCounter < 3
+			   )
+			{
+				yield return TextDisplayer.Instance.ShowUntilInput(
+					"I can't make it that easy for you! There's no fun if you destroy all this ice!"
+				);
+			}
+			else
+			{
+				yield return targetSlot.Card.TakeDamage(100, null);
+			}
 		}
 
 		yield return new WaitForSeconds(0.65f);
-		UseCounter++;
-	}
-
-	public override bool ExtraActivationPrerequisitesMet()
-	{
-		return UseCounter < 3 && GetValidTargets().Count > 0;
-	}
-
-	public override void OnExtraActivationPrerequisitesNotMet()
-	{
-		if (!_dialoguePlayed && UseCounter >= 3)
+		_useCounter++;
+		if (!_dialoguePlayed && _useCounter >= 3)
 		{
 			StartCoroutine(
 				TextDisplayer.Instance.ShowUntilInput("The was the last one, I hope you used it well.")
@@ -47,5 +53,10 @@ public class HammerItemExt : HammerItem
 
 			_dialoguePlayed = true;
 		}
+	}
+
+	public override bool ExtraActivationPrerequisitesMet()
+	{
+		return _useCounter < 3 && GetValidTargets().Count > 0;
 	}
 }
