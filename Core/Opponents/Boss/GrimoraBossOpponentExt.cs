bc7df22c57cd51f3ea182d71c728634b2ac7f422
julian-perge <julianperge@gmail.com>
2022-02-16 22:50:55 -0500

    Made not static

--- Core/Opponents/Boss/GrimoraBossOpponentExt.cs
+++ Core/Opponents/Boss/GrimoraBossOpponentExt.cs
@@ -102,7 +102,7 @@ public class GrimoraBossOpponentExt : BaseBossExt
 		yield break;
 	}
 
-	private static IEnumerator StartPlayerCardWeakeningPhase()
+	private IEnumerator StartPlayerCardWeakeningPhase()
 	{
 		var playerCardsThatAreValidToWeaken
 			= BoardManager.Instance
@@ -131,7 +131,7 @@ public class GrimoraBossOpponentExt : BaseBossExt
 		}
 	}
 
-	private static IEnumerator StartSpawningGiantsPhase()
+	private IEnumerator StartSpawningGiantsPhase()
 	{
 		var oppSlots = BoardManager.Instance.OpponentSlotsCopy;
 
@@ -143,15 +143,13 @@ public class GrimoraBossOpponentExt : BaseBossExt
 		ViewManager.Instance.SwitchToView(View.OpponentQueue, false, true);
 
 		// mimics the moon phase
-		CardInfo modifiedGiant = CreateModifiedGiant();
-
-		yield return BoardManager.Instance.CreateCardInSlot(modifiedGiant, oppSlots[1], 0.2f);
+		yield return BoardManager.Instance.CreateCardInSlot(CreateModifiedGiant(), oppSlots[1], 0.2f);
 		yield return new WaitForSeconds(0.5f);
-		yield return BoardManager.Instance.CreateCardInSlot(modifiedGiant, oppSlots[3], 0.2f);
+		yield return BoardManager.Instance.CreateCardInSlot(CreateModifiedGiant(), oppSlots[3], 0.2f);
 		yield return new WaitForSeconds(0.5f);
 	}
 
-	private static CardInfo CreateModifiedGiant()
+	private CardInfo CreateModifiedGiant()
 	{
 		CardInfo modifiedGiant = NameGiant.GetCardInfo();
 		modifiedGiant.abilities = new List<Ability>() { GiantStrike.ability, Ability.Reach };
@@ -190,7 +188,7 @@ public class GrimoraBossOpponentExt : BaseBossExt
 		}
 	}
 
-	private static CardInfo CreateModifiedBonelord()
+	private CardInfo CreateModifiedBonelord()
 	{
 		CardInfo bonelord = NameBonelord.GetCardInfo();
 		CardModificationInfo mod = new CardModificationInfo()
@@ -205,7 +203,7 @@ public class GrimoraBossOpponentExt : BaseBossExt
 		return bonelord;
 	}
 
-	private static CardInfo CreateModifiedBonelordsHorn()
+	private CardInfo CreateModifiedBonelordsHorn()
 	{
 		CardInfo bonelordsHorn = NameBoneLordsHorn.GetCardInfo();
 		bonelordsHorn.mods.Add(new CardModificationInfo() { attackAdjustment = 2 });
