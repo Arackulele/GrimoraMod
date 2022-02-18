5233091e57b6a5555290fe5b8b9a723de10a5a74
julian-perge <julianperge@gmail.com>
2022-02-18 16:36:29 -0500

    Separated prefabs constants into regions

--- Core/Utils/PrefabConstants.cs
+++ Core/Utils/PrefabConstants.cs
@@ -10,7 +10,20 @@ public static class PrefabConstants
 	public const string PathSpecialNodes = "Prefabs/SpecialNodeSequences";
 	public const string PathArt3D = "Art/Assets3D";
 
-	public static GameObject BoneyardFigurine = AssetUtils.GetPrefab<GameObject>("ChessboardPiece_Boneyard");
+
+	#region Cards
+
+	public static GameObject GrimoraCardBack =
+		ResourceBank.Get<GameObject>("Prefabs/Cards/CardBack_Grimora");
+
+	public static GameObject GrimoraPlayableCard =
+		ResourceBank.Get<GameObject>("Prefabs/Cards/PlayableCard_Grimora");
+
+	public static GameObject GrimoraSelectableCard =
+		ResourceBank.Get<GameObject>("Prefabs/Cards/SelectableCard_Grimora");
+
+	#endregion
+
 
 	#region BossPieces
 
@@ -28,37 +41,47 @@ public static class PrefabConstants
 
 	#endregion
 
+
+	#region ChessPieces
+
 	public static ChessboardChestPiece ChestPiece =
 		ResourceBank.Get<ChessboardChestPiece>($"{PathChessboardMap}/ChessboardChestPiece");
 
 	public static ChessboardEnemyPiece EnemyPiece =
 		ResourceBank.Get<ChessboardEnemyPiece>($"{PathChessboardMap}/ChessboardEnemyPiece");
 
-	public static GameObject GrimoraSelectableCard =
-		ResourceBank.Get<GameObject>("Prefabs/Cards/SelectableCard_Grimora");
+	public static GameObject CardRemovalFigurine = AssetUtils.GetPrefab<GameObject>("ChessboardPiece_CardRemove");
 
-	public static GameObject GrimoraPlayableCard =
-		ResourceBank.Get<GameObject>("Prefabs/Cards/PlayableCard_Grimora");
-
-	public static GameObject GrimoraCardBack =
-		ResourceBank.Get<GameObject>("Prefabs/Cards/CardBack_Grimora");
 
 	public static GameObject GoatEyeFigurine = AssetUtils.GetPrefab<GameObject>("ChessboardPiece_GoatEye");
 
-	public static GameObject CardRemovalFigurine = AssetUtils.GetPrefab<GameObject>("ChessboardPiece_CardRemove");
+	#endregion
+
+
+	#region Boneyard
+
+	public static GameObject BoneyardGrave = AssetUtils.GetPrefab<GameObject>("BoneyardBurialGrave");
+
+	public static GameObject BoneyardFigurine = AssetUtils.GetPrefab<GameObject>("ChessboardPiece_Boneyard");
 
 	public static GameObject Tombstone3 =
 		ResourceBank.Get<GameObject>($"{PathChessboardMap}/Chessboard_Tombstone_3");
 
-	public static GameObject CardStatBoostSequencer =
-		ResourceBank.Get<GameObject>($"{PathSpecialNodes}/CardStatBoostSequencer");
+	#endregion
 
-	public static GameObject BoneyardGrave = AssetUtils.GetPrefab<GameObject>("BoneyardBurialGrave");
+
+	#region ElectricChair
 
 	public static GameObject ElectricChair = AssetUtils.GetPrefab<GameObject>("ChessboardPiece_ElectricChair");
 
 	public static GameObject ElectricChairForSelectionSlot = AssetUtils.GetPrefab<GameObject>("ElectricChairV2");
 
+	#endregion
+	
+
+	public static GameObject CardStatBoostSequencer =
+		ResourceBank.Get<GameObject>($"{PathSpecialNodes}/CardStatBoostSequencer");
+	
 	public static Material WoodenBoxMaterial =
 		ResourceBank.Get<Material>($"{PathArt3D}/nodesequences/woodenbox/WoodenBox_Wood");
 
