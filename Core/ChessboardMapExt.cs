5d634468ec2da30e6ae47431cc0b5ef1d6569713
julian-perge <julianperge@gmail.com>
2022-02-18 16:49:52 -0500

    Increased time for showing pieces from 0.02 to 0.05.
    Added PlayerMarkerAnimator to Sawyer figurine

--- Core/ChessboardMapExt.cs
+++ Core/ChessboardMapExt.cs
@@ -292,7 +292,7 @@ public class ChessboardMapExt : GameMap
 			return toRemove;
 		});
 
-		yield return new WaitForSeconds(0.02f);
+		yield return new WaitForSeconds(0.05f);
 
 		yield return ShowPiecesThatAreActive();
 	}
@@ -302,7 +302,7 @@ public class ChessboardMapExt : GameMap
 		foreach (var piece in pieces.Where(piece => piece.gameObject.activeInHierarchy))
 		{
 			piece.Show();
-			yield return new WaitForSeconds(0.02f);
+			yield return new WaitForSeconds(0.05f);
 		}
 	}
 
