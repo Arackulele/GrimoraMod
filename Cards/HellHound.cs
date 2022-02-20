27048167bd5e020eba59bf2f3faef0175b588da1
Ara <73202349+Arackulele@users.noreply.github.com>
2022-02-20 13:11:25 +0100

    Sawyer Boss mechanic

---
+++ Cards/HellHound.cs
@@ -0,0 +1,23 @@
+using APIPlugin;
+using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameHellHound = "GrimoraMod_HellHound";
+
+	private void Add_HellHound()
+	{
+		NewCard.Add(CardBuilder.Builder
+			.SetAsRareCard()
+			.SetAbilities(Ability.WhackAMole, Ability.Reach, GainAttackBones.ability)
+			.SetBaseAttackAndHealth(1, 8)
+			.SetBoneCost(5)
+			.SetDescription("A rabid dog, killed one of my ghouls.")
+			.SetNames(NameHellHound, "Hell Hound")
+			.Build()
+		);
+	}
+}
+
