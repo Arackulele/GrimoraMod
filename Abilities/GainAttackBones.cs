6c5554c250529280d3c3cdab4702ea82f860dfa9
Ara <73202349+Arackulele@users.noreply.github.com>
2022-02-20 13:15:42 +0100

    Sawyer ability

---
+++ Abilities/GainAttackBones.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using APIPlugin;
+using DiskCardGame;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+public class GainAttackBones : AbilityBehaviour
+{
+	public static Ability ability;
+
+	public override Ability Ability => ability;
+
+	public override bool RespondsToTurnEnd(bool playerTurnEnd)
+	{
+		return base.Card != null && base.Card.OpponentCard != playerTurnEnd;
+	}
+
+
+	public override IEnumerator OnTurnEnd(bool playerTurnEnd)
+	{
+		int boneamount = Singleton<ResourcesManager>.Instance.PlayerBones;
+		,
+
+	public static NewAbility Create()
+	{
+		const string rulebookDescription =
+			"[creature] gains 1 attack for each bone the player currently has.";
+
+		return ApiUtils.CreateAbility<GainAttackBones>(
+			rulebookDescription, "GainAttackBones"
+		);
+	}
+}
