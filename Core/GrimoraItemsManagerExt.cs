83908fc7e5f7c2db643c06f3666f97e1c23b2fdf
julian-perge <julianperge@gmail.com>
2022-02-20 10:52:37 -0500

    Size does not change the box. Even though extents is 'obsolete', it increase the box on where you can click on the hammer

--- Core/GrimoraItemsManagerExt.cs
+++ Core/GrimoraItemsManagerExt.cs
@@ -43,8 +43,8 @@ public class GrimoraItemsManagerExt : ItemsManager
 			);
 
 			ext.hammerSlot = part3ItemsManager.hammerSlot;
-			Vector3 sizeCopy = ext.hammerSlot.GetComponent<BoxCollider>().size;
-			ext.hammerSlot.GetComponent<BoxCollider>().size = new Vector3(1f, sizeCopy.y, sizeCopy.z); 
+			Vector3 extentsCopy = ext.hammerSlot.GetComponent<BoxCollider>().extents;
+			ext.hammerSlot.GetComponent<BoxCollider>().extents = new Vector3(1f, extentsCopy.y, extentsCopy.z); 
 			part3ItemsManager.hammerSlot.transform.SetParent(ext.transform);
 
 			float xVal = ConfigHelper.Instance.HasIncreaseSlotsMod ? -8.75f : -7.5f;
