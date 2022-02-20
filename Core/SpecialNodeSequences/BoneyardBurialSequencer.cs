256baf9727b7441d85d889943edb999c5e05c7eb
julian-perge <julianperge@gmail.com>
2022-02-20 11:46:09 -0500

    Formatting

--- Core/SpecialNodeSequences/BoneyardBurialSequencer.cs
+++ Core/SpecialNodeSequences/BoneyardBurialSequencer.cs
@@ -107,16 +107,16 @@ public class BoneyardBurialSequencer : CardStatBoostSequencer
 		{
 			yield return TextDisplayer.Instance.ShowUntilInput(
 				"A LONE GRAVE SITS SOLEMNLY IN FRONT OF YOU.", -0.65f
-				);
+			);
 			yield return TextDisplayer.Instance.ShowUntilInput(
 				"IN FRONT OF IT IS A MOUND OF EARTH, LEFT BY SOMEONE WHO'S ALREADY PASSED ON.", -0.65f
-				);
+			);
 			yield return TextDisplayer.Instance.ShowUntilInput(
 				$"PERHAPS A MEMBER OF YOUR UNDEAD HORDE COULD {"DIG THEM UP?".BrightRed()}", -0.65f
-				);
+			);
 			yield return TextDisplayer.Instance.ShowUntilInput(
 				"THIS WOULDN'T BE WITHOUT REPERCUSSIONS OF COURSE, AS DEATH IS NEVER PERMANENT.", -0.65f
-				);
+			);
 
 			yield return confirmStone.WaitUntilConfirmation();
 			bool finishedBuffing = false;
