955753cfe2334f1beeff7b6b9f4db50a5754227b
julian-perge <julianperge@gmail.com>
2022-02-24 20:44:01 -0500

    DeathKnell_Bell added replacing DausBell
    New artwork for Dead Pets courtesy of Nevernamed

---
+++ Cards/DeathKnell_Bell.cs
@@ -0,0 +1,18 @@
+﻿using APIPlugin;
+using DiskCardGame;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameDeathKnellBell = "GrimoraMod_DeathKnell_Bell";
+
+	private void Add_DeathKnellBell()
+	{
+		new CustomCard("DausBell")
+		{
+			tex = AssetUtils.GetPrefab<Sprite>("DeathKnell_Bell").texture
+		};
+	}
+}
