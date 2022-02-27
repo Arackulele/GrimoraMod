d5d8e756d6efb9e1a97e3f53f411aa8a057b590f
Ara <73202349+Arackulele@users.noreply.github.com>
2022-02-27 19:37:10 +0100

    Hammer Dialogue Fix

--- Core/Models/HammerItemExt.cs
+++ Core/Models/HammerItemExt.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using DiskCardGame;
 using HarmonyLib;
 using Pixelplacement;
@@ -58,7 +58,7 @@ public class HammerItemExt : HammerItem
 
 		if (_useCounter == 1)
 		{
-			yield return PlayDialogue("THE FRAIL THING WILL SHATTER AFTER EXCESSIVE USE. THREE STRIKES, AND IT'S OUT.");
+			yield return PlayDialogue("THE FRAIL THING WILL SHATTER AFTER EXCESSIVE USE. THREE STRIKES, AND IT'S OUT, FOR THIS FIGHT AT LEAST.");
 			HammerHandleMat.SetFloat(Glossiness, 0.6f);
 		}
 		else if (_useCounter == 2)
