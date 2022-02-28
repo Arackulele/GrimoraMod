c3a4fe951de8e908cf2db7dcbfb59e269d243642
julian-perge <julianperge@gmail.com>
2022-02-28 12:32:49 -0500

    Added Hellhand

---
+++ Cards/Hellhand.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameHellhand = "GrimoraMod_Hellhand";
+
+	private void Add_Hellhand()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(Ability.LatchBrittle)
+			.SetBaseAttackAndHealth(1, 1)
+			.SetBoneCost(5)
+			.SetDescription("I WOULDN'T GET TOO CLOSE DEAR. YOU CAN'T BREAK THE HOLD ONCE IT LATCHES ON.")
+			.SetNames(NameHellhand, "Hellhand")
+			.Build();
+	}
+}
