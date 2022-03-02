9a09a4cd0dd22f20318f51e0d1a26deb0d127d79
julian-perge <julianperge@gmail.com>
2022-03-02 13:04:23 -0500

    Changed RaidingPirate to PirateSwashbuckler

---
+++ Cards/PirateSwashbuckler.cs
@@ -0,0 +1,15 @@
+﻿namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NamePirateSwashbuckler = "GrimoraMod_PirateSwashbuckler";
+
+	private void Add_PirateSwashbuckler()
+	{
+		CardBuilder.Builder
+			.SetAbilities(Raider.ability)
+			.SetBaseAttackAndHealth(1, 2)
+			.SetNames(NamePirateSwashbuckler, "Swashbuckler")
+			.Build();
+	}
+}
