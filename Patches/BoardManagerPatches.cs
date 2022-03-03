b0117f54b3886abdc858fe591747da43c33a0560
julian-perge <julianperge@gmail.com>
2022-03-03 14:09:39 -0500

    Added BoardManagerPatches for setting the queued card to the parent of the queued slot on the board. This is so that when the table sways in Royal's fight, it correctly stays with the board and not hover

---
+++ Patches/BoardManagerPatches.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(BoardManager))]
+public class BoardManagerPatches
+{
+	[HarmonyPostfix, HarmonyPatch(nameof(BoardManager.QueueCardForSlot))]
+	public static void PostfixAssignAsChildForQueuedCard(
+		PlayableCard card,
+		CardSlot slot,
+		float tweenLength = 0.1f,
+		bool doTween = true,
+		bool setStartPosition = true
+	)
+	{
+		card.transform.SetParent(BoardManager.Instance.OpponentQueueSlots[card.QueuedSlot.Index].transform);
+	}
+}
