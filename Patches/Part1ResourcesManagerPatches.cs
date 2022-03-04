48542285866543b0412e66593a6fdf5b31828883
julian-perge <julianperge@gmail.com>
2022-03-03 19:39:01 -0500

    Added patch to set the bone tokens a child of the boneTokenArea

---
+++ Patches/Part1ResourcesManagerPatches.cs
@@ -0,0 +1,18 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(Part1ResourcesManager))]
+public class Part1ResourcesManagerPatches
+{
+
+	[HarmonyPostfix, HarmonyPatch(nameof(Part1ResourcesManager.ShowAddBones))]
+	public static void SetTokensAsChildrenOfBoneTokenArea(Part1ResourcesManager __instance, int amount, CardSlot slot)
+	{
+		foreach (var boneToken in __instance.boneTokens.Where(boneToken => boneToken.transform.parent != __instance.boneTokenArea))
+		{
+			boneToken.transform.SetParent(__instance.boneTokenArea);
+		}
+	}
+}
