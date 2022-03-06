696b4fb862ffc228be3e34b8b8296fbab40be8bf
julian-perge <julianperge@gmail.com>
2022-03-06 16:04:52 -0500

    Commenting out patch for now as for some ungodly reason, icons will randomly flip themselves.

--- Patches/AbilityIconInteractablePatches.cs
+++ Patches/AbilityIconInteractablePatches.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 namespace GrimoraMod;
 
-[HarmonyPatch(typeof(AbilityIconInteractable))]
+// [HarmonyPatch(typeof(AbilityIconInteractable))]
 public class AbilityIconInteractablePatches
 {
 	[HarmonyPrefix, HarmonyPatch(nameof(AbilityIconInteractable.SetFlippedY))]
 	public static bool ChangeStupidNegativeYScalingLogWarning(AbilityIconInteractable __instance, bool flippedY)
 	{
-		var localScale = __instance.transform.localScale;
-		float y = 1f * Mathf.Abs(localScale.y);
-		__instance.transform.localScale = new Vector3(localScale.x, y, localScale.z);
 		if (flippedY)
 		{
-			__instance.transform.Rotate(new Vector3(0, 0, 180));
+			GrimoraPlugin.Log.LogDebug(
+				$"[AbilityIconPatch.Before] Card [{__instance.card.InfoName()}] Ability [{__instance.Ability}] Is Opponent Card [{__instance.card.OpponentCard}] FlippedY? [{flippedY}]"
+			);
+			GrimoraPlugin.Log.LogDebug(
+				$"[AbilityIconPatch.Before] Card [{__instance.card.InfoName()}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]"
+			);
+			float y = 1f * Mathf.Abs(__instance.transform.localScale.y);
+			__instance.transform.localScale = new Vector3(
+				__instance.transform.localScale.x,
+				y,
+				__instance.transform.localScale.z
+			);
+			__instance.transform.localRotation = Quaternion.Euler(0, 0, 180);
+			// // if (flippedY)
+			// // {
+			// // 	GrimoraPlugin.Log.LogDebug($"[AbilityIconPatch] Card [{__instance.card.name}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]");
+			// // 	__instance.transform.Rotate(new Vector3(0, 0, 180));
+			// // }
+			return false;
 		}
-		else
-		{
-			__instance.transform.rotation = Quaternion.identity;
-		}
-		return false;
+
+		return true;
 	}
-	
+
+	[HarmonyPostfix, HarmonyPatch(nameof(AbilityIconInteractable.SetFlippedY))]
+	public static void ChangeStupidNegativeYScalingLogWarningAfter(
+		AbilityIconInteractable __instance,
+		bool flippedY
+	)
+	{
+		if (flippedY)
+		{
+			GrimoraPlugin.Log.LogDebug(
+				$"[AbilityIconPatch.After ] Card [{__instance.card.InfoName()}] Ability [{__instance.Ability}] Is Opponent Card [{__instance.card.OpponentCard}] FlippedY? [{flippedY}]"
+			);
+			GrimoraPlugin.Log.LogDebug(
+				$"[AbilityIconPatch.After ] Card [{__instance.card.InfoName()}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]"
+			);
+		}
+		// float y = 1f * Mathf.Abs(__instance.transform.localScale.y);
+		// __instance.transform.localScale = new Vector3(__instance.transform.localScale.x, y, __instance.transform.localScale.z);
+		// // if (flippedY)
+		// // {
+		// // 	GrimoraPlugin.Log.LogDebug($"[AbilityIconPatch] Card [{__instance.card.name}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]");
+		// // 	__instance.transform.Rotate(new Vector3(0, 0, 180));
+		// // }
+		// return false;
+	}
 }
