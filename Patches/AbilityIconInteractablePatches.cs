822cc2895fb8057fb8a666323d187c06a5b54421
julian-perge <julianperge@gmail.com>
2022-03-06 14:41:33 -0500

    Added patch to fix the stupid log spam that Unity says with 'BoxColliders does not support negative scale or size'

---
+++ Patches/AbilityIconInteractablePatches.cs
@@ -0,0 +1,27 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(AbilityIconInteractable))]
+public class AbilityIconInteractablePatches
+{
+	[HarmonyPrefix, HarmonyPatch(nameof(AbilityIconInteractable.SetFlippedY))]
+	public static bool ChangeStupidNegativeYScalingLogWarning(AbilityIconInteractable __instance, bool flippedY)
+	{
+		var localScale = __instance.transform.localScale;
+		float y = 1f * Mathf.Abs(localScale.y);
+		__instance.transform.localScale = new Vector3(localScale.x, y, localScale.z);
+		if (flippedY)
+		{
+			__instance.transform.Rotate(new Vector3(0, 0, 180));
+		}
+		else
+		{
+			__instance.transform.rotation = Quaternion.identity;
+		}
+		return false;
+	}
+	
+}
