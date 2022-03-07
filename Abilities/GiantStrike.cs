f6784bc190cb2c04e948ce2013a9eb3a61f5a706
julian-perge <julianperge@gmail.com>
2022-03-06 19:26:26 -0500

    Updated description for Giant Strike

--- Abilities/GiantStrike.cs
+++ Abilities/GiantStrike.cs
@@ -12,9 +12,8 @@ public class GiantStrike : AbilityBehaviour
 	public static NewAbility Create()
 	{
 		const string rulebookDescription =
-			"[creature] will strike each opposing space that is occupied by a creature. "
-			+ "If only one creature is in the opposing spaces, this card will strike twice. "
-			+ "This card will strike directly if no creatures oppose it.";
+			"[creature] will strike each opposing space. "
+			+ "If only one creature is in the opposing spaces, this card will strike that creature twice. ";
 
 		return ApiUtils.CreateAbility<GiantStrike>(rulebookDescription, flipYIfOpponent: true);
 	}
