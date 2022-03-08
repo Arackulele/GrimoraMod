30b183dc3aa1e9467255166931265d26d890c313
julian-perge <julianperge@gmail.com>
2022-03-08 15:56:56 -0500

    Only do damage sequence if dmgDoneToPlayer is greater than zero

--- Patches/CombatPhaseManagerPatches.cs
+++ Patches/CombatPhaseManagerPatches.cs
@@ -24,16 +24,19 @@ public class CombatPhaseManagerPatches
 		{
 			yield return new WaitForSeconds(1f);
 			int dmgDoneToPlayer = slot.Card.GetComponent<StrikeAdjacentSlots>().damageDoneToPlayer;
-			Log.LogDebug($"[SlotAttackSequence.StrikeAdj] Dealing [{dmgDoneToPlayer}] to player");
-			yield return LifeManager.Instance.ShowDamageSequence(
-				dmgDoneToPlayer,
-				dmgDoneToPlayer,
-				!slot.Card.OpponentCard,
-				0.2f
-			);
+			if(dmgDoneToPlayer > 0)
+			{
+				Log.LogDebug($"[SlotAttackSequence.StrikeAdj] Dealing [{dmgDoneToPlayer}] to player");
+				yield return LifeManager.Instance.ShowDamageSequence(
+					dmgDoneToPlayer,
+					dmgDoneToPlayer,
+					!slot.Card.OpponentCard,
+					0.2f
+				);
 
-			Log.LogDebug($"[SlotAttackSequence.StrikeAdj] Subtracting [{dmgDoneToPlayer}] from DamageDealtThisPhase");
-			__instance.DamageDealtThisPhase -= dmgDoneToPlayer;
+				Log.LogDebug($"[SlotAttackSequence.StrikeAdj] Subtracting [{dmgDoneToPlayer}] from DamageDealtThisPhase");
+				__instance.DamageDealtThisPhase -= dmgDoneToPlayer;
+			}
 		}
 	}
 }
