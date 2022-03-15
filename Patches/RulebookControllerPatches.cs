7e12f1ce7b76a37e8d107cc5a1e2f56bbc3a7416
julian-perge <julianperge@gmail.com>
2022-03-15 07:07:49 -0400

    Add back RulebookControllerPatches so that the stat icon page can be correctly viewed.

---
+++ Patches/RulebookControllerPatches.cs
@@ -0,0 +1,84 @@
+using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(RuleBookController))]
+public class RulebookControllerPatches
+{
+	[HarmonyPrefix, HarmonyPatch(nameof(RuleBookController.Start))]
+	public static void ChangeRuleBookInfo(ref RuleBookController __instance)
+	{
+		if (GrimoraSaveUtil.isNotGrimora)
+		{
+			return;
+		}
+
+		GameObject grimoraRangePrefab = __instance.bookInfo.pageRanges[0].rangePrefab;
+		__instance.bookInfo = ResourceBank.Get<RuleBookInfo>("Data/rulebook/RuleBookInfo");
+		foreach (var pageRange in __instance.bookInfo.pageRanges)
+		{
+			pageRange.rangePrefab = grimoraRangePrefab;
+		}
+	}
+}
+
+[HarmonyPatch(typeof(PageContentLoader))]
+public class PageContentLoaderPatch
+{
+	[HarmonyPrefix, HarmonyPatch(nameof(PageContentLoader.LoadPage))]
+	public static bool Prefix(ref PageContentLoader __instance, RuleBookPageInfo pageInfo)
+	{
+		if (GrimoraSaveUtil.isNotGrimora || pageInfo.abilityPage)
+		{
+			return true;
+		}
+
+		if (__instance.currentPagePrefab != pageInfo.pagePrefab)
+		{
+			if (__instance.currentPageObj != null)
+			{
+				Object.Destroy(__instance.currentPageObj);
+			}
+
+			__instance.currentPageObj = Object.Instantiate(
+				pageInfo.pagePrefab,
+				__instance.transform.position,
+				__instance.transform.rotation,
+				__instance.transform
+			);
+		}
+
+
+		foreach (GameObject currentAdditiveObject in __instance.currentAdditiveObjects)
+		{
+			Object.Destroy(currentAdditiveObject);
+		}
+
+		__instance.currentAdditiveObjects.Clear();
+		foreach (var additivePrefab in pageInfo.additivePrefabs.Where(additivePrefab => additivePrefab))
+		{
+			GameObject gameObject = Object.Instantiate(
+				additivePrefab,
+				__instance.transform.position,
+				__instance.transform.rotation,
+				__instance.transform
+			);
+			gameObject.SetActive(true);
+			__instance.currentAdditiveObjects.Add(gameObject);
+		}
+
+		AbilityPage component = __instance.currentPageObj.GetComponent<AbilityPage>();
+		component.name = "Grimora_StatIconRulebookPage";
+		StatIconPage statIconPage = component.gameObject.AddComponent<StatIconPage>();
+		statIconPage.descriptionTextMesh = component.mainAbilityGroup.descriptionTextMesh;
+		statIconPage.iconRenderer = component.mainAbilityGroup.iconRenderer;
+		statIconPage.nameTextMesh = component.mainAbilityGroup.nameTextMesh;
+		statIconPage.headerTextMesh = component.headerTextMesh;
+		Object.Destroy(statIconPage.GetComponent<AbilityPage>());
+		__instance.currentPagePrefab = statIconPage.gameObject;
+		component.FillPage(pageInfo.headerText, pageInfo.ability, pageInfo.fillerAbilityIds);
+		return false;
+	}
+}
