344a76066fa9a5031b4ef09806fe3e981d5e84c6
julian-perge <julianperge@gmail.com>
2022-03-18 15:25:17 -0400

    Removed a bunch of code in CardSingleChoicesSequencer that really only applies to the other acts.

--- Patches/Cards/CardSingleChoicesSequencerPatches.cs
+++ Patches/Cards/CardSingleChoicesSequencerPatches.cs
@@ -25,48 +25,45 @@ public class CardSingleChoicesSequencerPatches
 		// TODO: this is an exact copy, minus the commented out code at (choice.CardInfo.IsNotNull()) block
 		if (SaveManager.saveFile.IsGrimora)
 		{
-			CardSingleChoicesSequencer choicesSequencer = __state;
 			CardChoicesNodeData choicesData = nodeData as CardChoicesNodeData;
-			if (StoryEventsData.EventCompleted(StoryEvent.CloverFound) && choicesSequencer.rerollInteractable.IsNotNull())
+			if (StoryEventsData.EventCompleted(StoryEvent.CloverFound) && __state.rerollInteractable)
 			{
-				choicesSequencer.rerollInteractable.gameObject.SetActive(true);
-				choicesSequencer.rerollInteractable.SetEnabled(false);
+				__state.rerollInteractable.gameObject.SetActive(true);
+				__state.rerollInteractable.SetEnabled(false);
 				CustomCoroutine.WaitThenExecute(1f, delegate { __state.rerollInteractable.SetEnabled(true); });
 			}
 
-			choicesSequencer.chosenReward = null;
+			__state.chosenReward = null;
 			int randomSeed = SaveManager.SaveFile.GetCurrentRandomSeed();
-			while (choicesSequencer.chosenReward.IsNull())
+			while (__state.chosenReward.IsNull())
 			{
-				List<CardChoice> choices;
-				if (choicesData.overrideChoices != null)
-				{
-					choices = choicesData.overrideChoices;
-				}
-				else
-				{
-					choices = choicesSequencer.choiceGenerator.GenerateChoices(choicesData, randomSeed);
-					randomSeed *= 2;
-				}
+				var choices = __state.choiceGenerator.GenerateChoices(choicesData, randomSeed);
+				randomSeed *= 2;
 
 				float x = (float)((choices.Count - 1) * 0.5 * -1.5);
-				choicesSequencer.selectableCards = choicesSequencer.SpawnCards(
-					choices.Count, choicesSequencer.transform, new Vector3(x, 5.01f, 0.0f)
+				__state.selectableCards = __state.SpawnCards(
+					choices.Count,
+					__state.transform,
+					new Vector3(x, 5.01f, 0.0f)
 				);
 
 				for (int i = 0; i < choices.Count; ++i)
 				{
 					CardChoice choice = choices[i];
-					SelectableCard card = choicesSequencer.selectableCards[i];
+					SelectableCard card = __state.selectableCards[i];
 					card.gameObject.SetActive(true);
 					card.SetParticlesEnabled(true);
 					card.SetEnabled(false);
 					card.ChoiceInfo = choice;
 					if (choice.CardInfo.IsNotNull())
 					{
-						card.Initialize(choice.CardInfo, choicesSequencer.OnRewardChosen,
-							choicesSequencer.OnCardFlipped, true,
-							choicesSequencer.OnCardInspected);
+						card.Initialize(
+							choice.CardInfo,
+							__state.OnRewardChosen,
+							__state.OnCardFlipped,
+							true,
+							__state.OnCardInspected
+						);
 						// if (i == 2 && card.Anim is GravestoneCardAnimationController)
 						// {
 						// 	card.Initialize(cardChoice.CardInfo, null, delegate
@@ -77,81 +74,41 @@ public class CardSingleChoicesSequencerPatches
 						// 	}, startFlipped: true);
 						// }
 					}
-					else if (choice.resourceType != ResourceType.None)
-					{
-						card.Initialize(choicesSequencer.GetCardbackTexture(choice),
-							choicesSequencer.OnRewardChosen,
-							choicesSequencer.OnCardFlipped,
-							choicesSequencer.OnCardInspected);
-					}
-					else if (choice.tribe != Tribe.None)
-					{
-						card.Initialize(choicesSequencer.GetCardbackTexture(choice),
-							choicesSequencer.OnRewardChosen,
-							choicesSequencer.OnCardFlipped,
-							choicesSequencer.OnCardInspected);
-					}
-
-					if (choice.isDeathcardChoice)
-					{
-						card.SetCardback(ResourceBank.Get<Texture>("Art/Cards/card_back_deathcard"));
-					}
 
 					card.SetFaceDown(true, true);
 					Vector3 position = card.transform.position;
-					card.transform.position = card.transform.position + Vector3.forward * 5f +
-					                          new Vector3(-0.5f + UnityEngine.Random.value * 1f, 0f, 0f);
+					card.transform.position = position
+					                          + Vector3.forward * 5f
+					                          + new Vector3(-0.5f + UnityEngine.Random.value * 1f, 0f, 0f);
 					Tween.Position(card.transform, position, 0.3f, 0.0f, Tween.EaseInOut);
-					Tween.Rotate(card.transform, new Vector3(0f, 0f, UnityEngine.Random.value * 1.5f), Space.Self, 0.4f, 0f,
-						Tween.EaseOut);
+					Tween.Rotate(
+						card.transform,
+						new Vector3(0f, 0f, UnityEngine.Random.value * 1.5f),
+						Space.Self,
+						0.4f,
+						0f,
+						Tween.EaseOut
+					);
 					yield return new WaitForSeconds(0.2f);
 					ParticleSystem componentInChildren = card.GetComponentInChildren<ParticleSystem>();
-					if (componentInChildren.IsNotNull())
+					if (componentInChildren)
 					{
-						var emmision = componentInChildren.emission;
-						emmision.rateOverTime = 0f;
+						var emission = componentInChildren.emission;
+						emission.rateOverTime = 0f;
 					}
 				}
 
 				yield return new WaitForSeconds(0.2f);
-				if (choicesData.choicesType == CardChoicesType.Cost &&
-				    !ProgressionData.LearnedMechanic(MechanicsConcept.CostBasedCardChoice))
-				{
-					yield return TextDisplayer.Instance.PlayDialogueEvent(
-						"TutorialCostBasedChoice",
-						TextDisplayer.MessageAdvanceMode.Input
-					);
-					yield return new WaitForSeconds(0.2f);
-				}
-				else if (choicesData.choicesType == CardChoicesType.Tribe &&
-				         !ProgressionData.LearnedMechanic(MechanicsConcept.TribeBasedCardChoice))
-				{
-					yield return TextDisplayer.Instance.PlayDialogueEvent(
-						"TutorialTribeBasedChoice",
-						TextDisplayer.MessageAdvanceMode.Input
-					);
-					yield return new WaitForSeconds(0.2f);
-				}
-				else if (choicesData.choicesType == CardChoicesType.Deathcard &&
-				         !ProgressionData.LearnedMechanic(MechanicsConcept.DeathcardCardChoice))
-				{
-					yield return TextDisplayer.Instance.PlayDialogueEvent(
-						"TutorialDeathcardChoice",
-						TextDisplayer.MessageAdvanceMode.Input
-					);
-					yield return new WaitForSeconds(0.2f);
-					ProgressionData.SetMechanicLearned(MechanicsConcept.DeathcardCardChoice);
-				}
 
-				choicesSequencer.SetCollidersEnabled(true);
-				choicesSequencer.choicesRerolled = false;
-				choicesSequencer.EnableViewDeck(choicesSequencer.viewControlMode, choicesSequencer.basePosition);
+				__state.SetCollidersEnabled(true);
+				__state.choicesRerolled = false;
+				__state.EnableViewDeck(__state.viewControlMode, __state.basePosition);
 				yield return new WaitUntil(() => __state.chosenReward.IsNotNull() || __state.choicesRerolled);
-				choicesSequencer.DisableViewDeck();
-				choicesSequencer.CleanUpCards();
+				__state.DisableViewDeck();
+				__state.CleanUpCards();
 			}
 
-			yield return choicesSequencer.AddCardToDeckAndCleanUp(choicesSequencer.chosenReward);
+			yield return __state.AddCardToDeckAndCleanUp(__state.chosenReward);
 		}
 		else
 		{
