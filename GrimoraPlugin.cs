9d422369d64c3a08faab78adfdea11a11b27d015
julian-perge <julianperge@gmail.com>
2022-03-18 04:05:09 -0400

    Version 2.8.2 instead of 2.9.0 since it's only an api update

--- GrimoraPlugin.cs
+++ GrimoraPlugin.cs
@@ -15,7 +15,7 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 {
 	public const string GUID = "arackulele.inscryption.grimoramod";
 	public const string Name = "GrimoraMod";
-	private const string Version = "2.9.0";
+	private const string Version = "2.8.2";
 
 	internal static ManualLogSource Log;
 
