ff225bb4602dea99bbf2b447903ba5c0b27454c7
julian-perge <julianperge@gmail.com>
2022-03-19 04:16:37 -0400

    Added AbilityMetaCategory.GrimoraRulebook to all regular abilities

--- Core/Utils/ApiUtils.cs
+++ Core/Utils/ApiUtils.cs
@@ -30,7 +30,8 @@ namespace GrimoraMod
 			info.canStack = canStack;
 			info.metaCategories = new List<AbilityMetaCategory>()
 			{
-				AbilityMetaCategory.Part1Modular, AbilityMetaCategory.Part1Rulebook
+				AbilityMetaCategory.Part1Modular, AbilityMetaCategory.Part1Rulebook,
+				AbilityMetaCategory.GrimoraRulebook
 			};
 
 			return info;
