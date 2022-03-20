194f5b2369849220f1d2161e0a890ab1487636cb
julian-perge <julianperge@gmail.com>
2022-03-20 13:44:37 -0400

    Added Rot tail on hit card

---
+++ Cards/Rot_Tail_TwitchingArm.cs
@@ -0,0 +1,18 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Card;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameRotTail = $"{GUID}_Rot_tail";
+
+	private void Add_RotTail()
+	{
+		CardBuilder.Builder
+			.SetBaseAttackAndHealth(0, 1)
+			// .SetDescription("A SENSE OF DREAD CONSUMES YOU AS YOU REALIZE YOU ARE NOT ALONE IN THESE WOODS.")
+			.SetNames(NameRotTail, "Twitching Arm")
+			.Build();
+	}
+}
