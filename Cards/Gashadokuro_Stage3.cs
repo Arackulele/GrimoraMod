9bf73557f77b03b45f8583ad8f0280449afbc074
julian-perge <julianperge@gmail.com>
2022-03-20 13:44:53 -0400

    Added Gashadokuro card stages

---
+++ Cards/Gashadokuro_Stage3.cs
@@ -0,0 +1,18 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameGashadokuro = $"{GUID}_Gashadokuro";
+
+	private void Add_Gashadokuro_Stage3()
+	{
+		CardBuilder.Builder
+			.SetAbilities(Ability.TriStrike)
+			.SetBaseAttackAndHealth(2, 3)
+			// .SetDescription("THE CYCLE OF THE MUMMY LORD, NEVER ENDING.")
+			.SetNames(NameGashadokuro, "Gashadokuro")
+			.Build();
+	}
+}
