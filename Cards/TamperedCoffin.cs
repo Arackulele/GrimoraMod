3077e51a76f3b7c0e764d7f756ff9f7525d724f5
julian-perge <julianperge@gmail.com>
2022-03-20 13:46:59 -0400

    Added TamperedCoffin

---
+++ Cards/TamperedCoffin.cs
@@ -0,0 +1,15 @@
+﻿namespace GrimoraMod;
+public partial class GrimoraPlugin
+{
+	public const string NameTamperedCoffin = $"{GUID}_TamperedCoffin";
+
+	private void Add_TamperedCoffin()
+	{
+		CardBuilder.Builder
+			.SetAbilities(Boneless.ability)
+			.SetBaseAttackAndHealth(0, 1)
+			// .SetDescription("FOR WHOM THE BELL TOLLS?")
+			.SetNames(NameTamperedCoffin, "Tampered Coffin")
+			.Build();
+	}
+}
