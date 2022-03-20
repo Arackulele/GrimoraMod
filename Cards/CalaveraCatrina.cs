a57933bde0c37f8fc395dc7811d9ab8bf39d5fda
julian-perge <julianperge@gmail.com>
2022-03-20 13:49:05 -0400

    added Calavera Catrina

---
+++ Cards/CalaveraCatrina.cs
@@ -0,0 +1,20 @@
+using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameCalaveraCatrina = $"{GUID}_CalaveraCatrina";
+
+	private static void Add_CalaveraCatrina()
+	{
+		CardBuilder.Builder
+			.SetAsRareCard()
+			.SetAbilities(MarchingDead.ability)
+			.SetBaseAttackAndHealth(2, 2)
+			.SetBoneCost(8)
+			.SetNames(NameCalaveraCatrina, "Calavera Catrina")
+			// .SetDescription("There will be quite a bit of difficulty putting it back togethe- OW!")
+			.Build();
+	}
+}
