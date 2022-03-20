bac5e88bfbb65393a1ede366e977a8ec0542daa0
julian-perge <julianperge@gmail.com>
2022-03-20 13:50:59 -0400

    Added Gravebard

---
+++ Cards/Gravebard.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameGravebard = $"{GUID}_Gravebard";
+
+	private void Add_Gravebard()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(Ability.BuffNeighbours)
+			.SetBaseAttackAndHealth(1, 1)
+			.SetBoneCost(4)
+			.SetDescription("THE SINGING IS SO BAD IT RAISES THE DEAD...")
+			.SetNames(NameGravebard, "Gravebard")
+			.Build();
+	}
+}
