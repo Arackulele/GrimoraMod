2bcac41e70f154db3292dc62cef9b64a35285be6
julian-perge <julianperge@gmail.com>
2022-03-20 13:41:25 -0400

    Added Kennel and Shipwreck terrain

---
+++ Cards/Terrain/Kennel.cs
@@ -0,0 +1,19 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameKennel = $"{GUID}_Kennel";
+
+	private void Add_Kennel()
+	{
+		CardBuilder.Builder
+			.SetAbilities(Ability.IceCube)
+			.SetBaseAttackAndHealth(0, 2)
+			.SetIceCube(NameBonehound)
+			.SetNames(NameKennel, "Kennel")
+			.SetTraits(Trait.Structure, Trait.Terrain)
+			.Build();
+	}
+}
---
+++ Cards/Terrain/Shipwreck.cs
@@ -0,0 +1,18 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameShipwreck = $"{GUID}_Shipwreck";
+
+	private void Add_Shipwreck()
+	{
+		CardBuilder.Builder
+			.SetAbilities(Ability.Sharp)
+			.SetBaseAttackAndHealth(0, 3)
+			.SetNames(NameShipwreck, "Shipwreck")
+			.SetTraits(Trait.Structure, Trait.Terrain)
+			.Build();
+	}
+}
