d15f31475ec18fa27e618ecb5d3de9ece3d89320
julian-perge <julianperge@gmail.com>
2022-03-20 13:40:39 -0400

    Added LooseLimb, a thematic TailOnHit

---
+++ Abilities/Normal/LooseLimb.cs
@@ -0,0 +1,19 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Card;
+
+namespace GrimoraMod;
+
+public class LooseLimb : TailOnHit
+{
+	public static Ability ability;
+	
+	public override Ability Ability => ability;
+	
+	public static AbilityManager.FullAbility Create()
+	{
+		const string rulebookDescription =
+			"When [creature] would be struck, a Tail is created in its place and [creature] moves into an adjacent open slot.";
+
+		return ApiUtils.CreateAbility<LooseLimb>(rulebookDescription);
+	}
+}
