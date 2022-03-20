f51357f8904351ab616150b9f5a80e424ae4cd87
julian-perge <julianperge@gmail.com>
2022-03-20 13:52:01 -0400

    Added Slinger's Soul

---
+++ Cards/SlingersSoul.cs
@@ -0,0 +1,21 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameSlingersSoul = $"{GUID}_SlingersSoul";
+
+	private void Add_SlingersSoul()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(ActivatedDealDamageGrimora.ability)
+			.SetBaseAttackAndHealth(2, 3)
+			.SetBoneCost(4)
+			.SetEnergyCost(3)
+			.SetDescription("I WOULDN'T GET TOO CLOSE DEAR. YOU CAN'T BREAK THE HOLD ONCE IT LATCHES ON.")
+			.SetNames(NameSlingersSoul, "Slinger's Soul")
+			.Build();
+	}
+}
