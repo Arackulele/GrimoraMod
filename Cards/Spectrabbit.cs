eb5344c194b399c2832fc38c6b2897787a728ecb
julian-perge <julianperge@gmail.com>
2022-03-20 13:35:16 -0400

    Added patch for Draw Rabbits to use Spectrabbit

---
+++ Cards/Spectrabbit.cs
@@ -0,0 +1,16 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameSpectrabbit = $"{GUID}_Spectrabbit";
+
+	private void Add_Spectrabbit()
+	{
+		CardBuilder.Builder
+			.SetBaseAttackAndHealth(0, 1)
+			.SetNames(NameSpectrabbit, "Spectrabbit")
+			.Build();
+	}
+}
---
+++ Patches/Abilities/DrawRabbitsPatches.cs
@@ -0,0 +1,23 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(DrawRabbits))]
+public class DrawRabbitsPatches
+{
+
+	[HarmonyPrefix, HarmonyPatch(nameof(DrawRabbits.CardToDraw))]
+	public static bool ChangeDefaultRabbitToSpectrabbit(DrawRabbits __instance, ref CardInfo __result)
+	{
+		if (GrimoraSaveUtil.isNotGrimora)
+		{
+			return true;
+		}
+
+		CardInfo cardByName = GrimoraPlugin.NameSpectrabbit.GetCardInfo();
+		cardByName.Mods.AddRange(__instance.GetNonDefaultModsFromSelf(__instance.Ability));
+		__result = cardByName;
+		return false;
+	}
+}
