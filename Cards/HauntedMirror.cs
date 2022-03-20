f1541ee140d29c8e34a5d8ce8dd07d1469f008ac
julian-perge <julianperge@gmail.com>
2022-03-20 13:49:40 -0400

    Added Haunted Mirror

---
+++ Cards/HauntedMirror.cs
@@ -0,0 +1,21 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameHauntedMirror = $"{GUID}_HauntedMirror";
+
+	private void Add_HauntedMirror()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetSpecialAbilities(SpecialTriggeredAbility.Mirror)
+			.SetBaseAttackAndHealth(0, 2)
+			.SetEnergyCost(4)
+			// .SetDescription("")
+			.SetNames(NameHauntedMirror, "Haunted Mirror")
+			.SetSpecialStatIcon(SpecialStatIcon.Mirror)
+			.Build();
+	}
+}
