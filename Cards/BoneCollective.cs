a3fcf9d4f2d61bf3c0595a3fd1fa5cd932c9d1f5
julian-perge <julianperge@gmail.com>
2022-03-20 13:50:52 -0400

    Added Bone Collective

---
+++ Cards/BoneCollective.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameBoneCollective = $"{GUID}_BoneCollective";
+
+	private void Add_BoneCollective()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(Ability.ActivatedStatsUp, Ability.Submerge)
+			.SetBaseAttackAndHealth(0, 1)
+			.SetBoneCost(2)
+			.SetNames(NameBoneCollective, "Bone Collective")
+			// .SetDescription("NOT YOUR ORDINARY UNDEAD, THEY SEARCHED THROUGH A SCRAPYARD FOR THIS GEAR.")
+			.Build();
+	}
+}
