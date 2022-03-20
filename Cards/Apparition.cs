edb33fc9df04dd11d6ff490bbc1e96d57c564ed5
julian-perge <julianperge@gmail.com>
2022-03-20 13:41:45 -0400

    Replaced Amoeba with Apparition

---
+++ Cards/Apparition.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameApparition = $"{GUID}_Apparition";
+
+	private void Add_Apparition()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(GrimoraRandomAbility.ability)
+			.SetBaseAttackAndHealth(1, 2)
+			.SetEnergyCost(4)
+			.SetDescription("The amorphous Apparition. Its sigils are ever changing.")
+			.SetNames(NameApparition, "Apparition")
+			.Build();
+	}
+}
