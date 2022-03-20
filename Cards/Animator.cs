4a92e69fffa73f1eac7a2195b32f2c3917385512
julian-perge <julianperge@gmail.com>
2022-03-20 13:44:11 -0400

    Added Animator

---
+++ Cards/Animator.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameAnimator = $"{GUID}_Animator";
+
+	private void Add_Animator()
+	{
+		CardBuilder.Builder
+			.SetAsRareCard()
+			.SetAbilities(Puppeteer.ability)
+			.SetBaseAttackAndHealth(1, 1)
+			.SetEnergyCost(5)
+			.SetDescription("The puppet becomes the puppeteer, yet still forever cursed by their dangling restraints.")
+			.SetNames(NameAnimator, "Animator")
+			.Build();
+	}
+}
