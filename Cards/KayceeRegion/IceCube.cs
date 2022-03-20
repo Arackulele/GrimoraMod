cc3f14a726706f66238b5e51771a348cfe80c291
julian-perge <julianperge@gmail.com>
2022-03-20 13:43:57 -0400

    Added IceCube for Kaycee region

---
+++ Cards/KayceeRegion/IceCube.cs
@@ -0,0 +1,18 @@
+﻿namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameIceCube = $"{GUID}_IceCube";
+
+	private void Add_IceCube()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(ColdFront.ability)
+			.SetBaseAttackAndHealth(1, 1)
+			.SetBoneCost(5)
+			// .SetDescription("THAT'S WHAT HAPPENS WHEN YOU PLAY WITH BOMBS!")
+			.SetNames(NameIceCube, "Ice Cube")
+			.Build();
+	}
+}
