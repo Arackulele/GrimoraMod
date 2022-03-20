4f7a0dd78aad559dda967b76f921d93f2a73fa77
julian-perge <julianperge@gmail.com>
2022-03-20 13:39:30 -0400

    Added ColdFront sigil

---
+++ Abilities/Normal/ColdFront.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using DiskCardGame;
+using InscryptionAPI.Card;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+public class ColdFront : AbilityBehaviour
+{
+	public static Ability ability;
+
+	public override Ability Ability => ability;
+
+	public override bool RespondsToDie(bool wasSacrifice, PlayableCard killer)
+	{
+		return true;
+	}
+
+	public override IEnumerator OnDie(bool wasSacrifice, PlayableCard killer)
+	{
+		PlayableCard opposingSlotCard = Card.Slot.opposingSlot.Card;
+		if (opposingSlotCard)
+		{
+			var frozenAway = GrimoraModKayceeBossSequencer.CreateModForFreeze(opposingSlotCard);
+			opposingSlotCard.RemoveAbilityFromThisCard(frozenAway);
+			opposingSlotCard.Anim.PlayTransformAnimation();
+			yield return new WaitForSeconds(0.25f);
+		}
+	}
+
+	public static AbilityManager.FullAbility Create()
+	{
+		const string rulebookDescription = "When [creature] perishes, the card opposing it is Frozen Away.";
+
+		return ApiUtils.CreateAbility<ColdFront>(rulebookDescription);
+	}
+}
