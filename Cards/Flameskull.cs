59776d9246f5ed17866f3d0d94f06332f41de52b
julian-perge <julianperge@gmail.com>
2022-03-20 13:48:11 -0400

    Added Flameskull

---
+++ Cards/Flameskull.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameFlameskull = $"{GUID}_Flameskull";
+
+	private void Add_Flameskull()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(Ability.BuffNeighbours, Ability.Flying)
+			.SetBaseAttackAndHealth(2, 1)
+			.SetBoneCost(6)
+			.SetNames(NameFlameskull, "Flameskull")
+			.SetDescription("THE BLAZING FLAMESKULL, ITS HEAT ENHANCES CREATURES ADJACENT TO IT.")
+			.Build();
+	}
+}
