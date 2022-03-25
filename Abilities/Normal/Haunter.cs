8f25995a7fab4e4c2e86303d8625331896ae2192
julian-perge <julianperge@gmail.com>
2022-03-25 05:28:38 -0400

    Refactored and cleaned up Haunter

--- Abilities/Normal/Haunter.cs
+++ Abilities/Normal/Haunter.cs
@@ -1,88 +1,29 @@
 ﻿using System.Collections;
 using DiskCardGame;
-using Pixelplacement;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using InscryptionAPI.Card;
+using Sirenix.Utilities;
 using UnityEngine;
 
 namespace GrimoraMod;
 
-// TODO: The commented out code does work, minus the rendering fix. Will take some time to understand.
 public class Haunter : AbilityBehaviour
 {
 	public static Ability ability;
-	private static readonly int Color1 = Shader.PropertyToID("_Color");
 
 	public override Ability Ability => ability;
 
 	public override bool RespondsToPreDeathAnimation(bool wasSacrifice)
 	{
-		return Card.Info.Abilities.FindAll(ab => ab != ability).Count > 0;
+		return Card.Info.Abilities.Any(ab => ab != ability);
 	}
 
 	public override IEnumerator OnPreDeathAnimation(bool wasSacrifice)
 	{
-		List<GameObject> createdSigils = new List<GameObject>();
-		List<AbilityIconInteractable> icons = Card.GetComponentInChildren<CardAbilityIcons>().abilityIcons;
-		foreach (AbilityIconInteractable icon in icons)
+		// greater than 1 because if it has Haunter, it must have at least 1 more sigil in order to transfer.
+		// Haunting doesn't work if only Haunter exists on the card.
+		if (Card.Info.abilities.Count > 1)
 		{
-			if (icon.Ability == ability) continue;
-
-			//Get and Spawn Sigils
-			AbilityIconInteractable abilityIcon = Instantiate(
-				icon,
-				icon.transform.position + new Vector3(0, 0.1f, 0),
-				Quaternion.identity,
-				Card.Slot.transform
-			);
-			abilityIcon.name = GrimoraMod.PrintUtils.GetAbilityName(abilityIcon.Ability);
-
-			// foreach (Renderer rend in sigils.GetComponentsInChildren<Renderer>())
-			// {
-			// 	rend.enabled = true;
-			// 	rend.material = new Material(rend.material);
-			// }
-			//
-			// Renderer sigilRenderer = sigils.GetComponent<Renderer>();
-			// Texture tex = sigilRenderer.material.mainTexture;
-			// sigilRenderer.material = Card.GetComponentInChildren<CardAbilityIcons>().emissiveIconMat;
-			// sigilRenderer.material.mainTexture = tex;
-			// sigilRenderer.material.SetColor(Color1, GameColors.Instance.glowSeafoam);
-
-			// //Handle Rotation
-			// int iterator = 0;
-			// foreach (Transform t in sigils.GetComponentsInChildren<Transform>())
-			// {
-			// 	t.localRotation = Quaternion.identity;
-			// 	t.localScale = icon.transform.GetComponentsInChildren<Transform>()[iterator].transform.localScale;
-			// 	t.localScale = new Vector3((t.localScale.x * 1.2f), (t.localScale.x * 1.2f), t.localScale.z);
-			// 	iterator++;
-			// }
-
-			// sigils.transform.rotation = Card.Slot.transform.rotation;
-			// sigils.transform.Rotate(Vector3.left, -90f);
-
-			//Fix Rend
-			// List<CardAbilityIcons> cardAbilityIcons = sigils.GetComponentsInParent<CardAbilityIcons>().ToList();
-			// cardAbilityIcons.AddRange(sigils.GetComponentsInChildren<CardAbilityIcons>().ToList());
-			// foreach (var i in cardAbilityIcons.Where(cai => cai.IsNotNull()))
-			// {
-			// 	DestroyImmediate(i);
-			// }
-
-			abilityIcon.gameObject.SetActive(true);
-			createdSigils.Add(abilityIcon.gameObject);
-		}
-
-		if (createdSigils.Count > 0)
-		{
-			HauntedSlot hauntedSlot = Card.Slot.gameObject.AddComponent<HauntedSlot>();
-			hauntedSlot.Init(Card.Slot, createdSigils, Card.Info.Abilities.FindAll(x => x != ability));
-
+			HauntedSlot hauntedSlot = HauntedSlot.SetupSlot(Card);
 			GlobalTriggerHandler.Instance.RegisterNonCardReceiver(hauntedSlot);
 		}
 
@@ -92,7 +33,7 @@ public class Haunter : AbilityBehaviour
 	public static AbilityManager.FullAbility Create()
 	{
 		const string rulebookDescription = "When [creature] perishes, it haunts the space it died in. "
-		                                   + "Creatures played on this space gain its old sigils.";
+		                                   + "The first creature played on this space gain its old sigils.";
 
 		return ApiUtils.CreateAbility<Haunter>(
 			rulebookDescription,
@@ -104,24 +45,52 @@ public class Haunter : AbilityBehaviour
 public class HauntedSlot : NonCardTriggerReceiver
 {
 	[SerializeField] public CardSlot cardSlot;
-	public List<GameObject> hauntSigils;
-	public List<Ability> abilities;
+	[SerializeField] public List<Ability> abilities = new();
+	private CardModificationInfo _modInfo;
 
-	public void Init(CardSlot slot, List<GameObject> visualSigils, List<Ability> actualSigils)
+	public static HauntedSlot SetupSlot(PlayableCard playableCard)
 	{
-		hauntSigils ??= new List<GameObject>(visualSigils);
+		Material defaultIconMat = playableCard.AbilityIcons.defaultIconMat;
+		HauntedSlot hauntedSlot = new GameObject(playableCard.Slot.name + "_Haunted").AddComponent<HauntedSlot>();
+		hauntedSlot.transform.SetParent(playableCard.Slot.transform);
+		hauntedSlot.cardSlot = playableCard.Slot;
+		hauntedSlot.abilities = playableCard.AbilityIcons.abilityIcons
+			.Where(abilityIconInteractable => abilityIconInteractable.Ability != Haunter.ability)
+			.ForEach(icon =>
+			{
+				AbilityIconInteractable hauntedIcon = Instantiate(
+					icon,
+					icon.transform.position + new Vector3(0, 0.1f),
+					Quaternion.Euler(90, 0, 0),
+					hauntedSlot.transform
+				);
+				hauntedIcon.name = AbilityManager.AllAbilities.Find(fa => fa.Id == icon.Ability).Info.rulebookName;
+				Renderer renderer = hauntedIcon.GetComponent<MeshRenderer>();
+				renderer.enabled = true;
+				// this is so that it doesn't appear behind the card slot texture at the lowest point in the wave movement 
+				renderer.sortingOrder = 1;
+				hauntedIcon.SetMaterial(defaultIconMat);
+				hauntedIcon.AssignAbility(icon.Ability, playableCard.Info, null);
 
-		abilities ??= new List<Ability>(actualSigils);
+				SineWaveMovement wave = hauntedIcon.gameObject.AddComponent<SineWaveMovement>();
+				wave.speed = 1;
+				wave.xMagnitude = 0;
+				wave.yMagnitude = 0.1f;
+				wave.zMagnitude = 0;
 
-		cardSlot = slot;
+				hauntedSlot.abilities.Add(icon.Ability);
+			})
+			.Select(icon => icon.Ability)
+			.ToList();
 
-		foreach (var wave in hauntSigils.Select(sigil => sigil.AddComponent<SineWaveMovement>()))
+		hauntedSlot._modInfo = new CardModificationInfo
 		{
-			wave.speed = 1;
-			wave.xMagnitude = 0;
-			wave.yMagnitude = 0.1f;
-			wave.zMagnitude = 0;
-		}
+			abilities = new List<Ability>(hauntedSlot.abilities),
+			fromCardMerge = false,
+			fromTotem = false
+		};
+
+		return hauntedSlot;
 	}
 
 	public override bool RespondsToOtherCardAssignedToSlot(PlayableCard otherCard)
@@ -131,21 +100,12 @@ public class HauntedSlot : NonCardTriggerReceiver
 
 	public override IEnumerator OnOtherCardAssignedToSlot(PlayableCard otherCard)
 	{
-		otherCard.AddTemporaryMod(
-			new CardModificationInfo
-			{
-				abilities = new List<Ability>(abilities), fromCardMerge = false, fromTotem = false
-			}
-		);
+		otherCard.AddTemporaryMod(_modInfo);
 		otherCard.Anim.PlayTransformAnimation();
-		otherCard.RenderCard();
+		otherCard.UpdateStatsText();
 
-		foreach (var haunt in hauntSigils.Where(haunt => haunt.IsNotNull()))
-		{
-			CustomCoroutine.WaitThenExecute(0.1f, delegate() { Destroy(haunt); });
-		}
-
-		CustomCoroutine.WaitThenExecute(0.1f, delegate() { Destroy(this); });
+		GrimoraPlugin.Log.LogDebug($"Destroying HauntedSlot [{this}]");
+		CustomCoroutine.WaitThenExecute(0.1f, delegate() { Destroy(this.gameObject); });
 		yield break;
 	}
 }
