543bde02d63defa0735804354117636c74f335a6
julian-perge <julianperge@gmail.com>
2022-03-27 11:38:27 -0400

    Removed old ability/card addition

--- GrimoraPlugin.cs
+++ GrimoraPlugin.cs
@@ -72,42 +72,6 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 		LoadAbilitiesAndCards();
 	}
 
-	private void LoadAbilities()
-	{
-		Log.LogDebug($"Loading abilities");
-
-		// Anchored.Create();                     // Blind, the Bound Demon
-		// ActivatedDrawSkeletonGrimora.Create(); // Blind, the Bound Demon
-		// ActivatedEnergyDrawWyvern.Create();    // Blind, the Bound Demon
-		// AlternatingStrike.Create();     // Bt Y#0895
-		// AreaOfEffectStrike.Create();    // Bt Y#0895
-		// BonelordsReign.Create();        // Ara
-		// BuffCrewMates.Create();         // Blind, the Bound Demon
-		// CreateArmyOfSkeletons.Create(); // Blind, the Bound Demon
-		// Erratic.Create();               // Bt Y#0895
-		// FlameStrafe.Create();           // Cevin
-		// GiantStrike.Create();           // Blind, the Bound Demon
-		// GiantStrikeEnraged.Create();    // Blind, the Bound Demon
-		// GrimoraRandomAbility.Create();  // vanilla
-		// Haunter.Create();               // vanilla
-		// HookLineAndSinker.Create();     // Bt Y#0895
-		// InvertedStrike.Create();        // Bt Y#0895
-		// LitFuse.Create();
-		// Possessive.Create();   // Bt Y#0895
-		// Raider.Create();       // Blind, the Bound Demon
-		// SkinCrawler.Create();  // Bt Y#0895
-		// SpiritBearer.Create(); // Bt Y#0895
-
-		#region Special
-
-		// CreateRoyalsCrewMate.Create();
-		// GainAttackBones.Create(); // vanilla
-		// GrimoraGiant.Create();
-		// LammergeierAttack.Create(); // vanilla
-
-		#endregion
-	}
-
 	private void LoadAbilitiesAndCards()
 	{
 		Log.LogDebug($"Loading cards");
@@ -124,81 +88,9 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 		var allAddMethods = AccessTools.GetDeclaredMethods(typeof(GrimoraPlugin))
 		 .Where(mi => mi.Name.StartsWith("Add_"))
 		 .ToList();
-		Log.LogInfo($"Adding [{allAddMethods.Count}] cards.");
 		allAddMethods.Sort((mi, mi2) => string.Compare(mi.Name, mi2.Name, StringComparison.Ordinal));
 		allAddMethods.ForEach(mi => mi.Invoke(this, null));
-
-		#region Normal
-
-		// Add_Banshee();                  // vanilla
-		// Add_BonePrince();               // Cevin2006™ (◕‿◕)#7971
-		// Add_Bonehound();                // vanilla
-		// Add_Bonelord();                 // Ryan S Art
-		// Add_Bonepile();                 // Bt Y#0895
-		// Add_BooHag();                   // Bt Y#0895
-		// Add_Catacomb();                 // Bt Y#0895
-		// Add_Centurion();                // Bt Y#0895
-		// Add_CompoundFracture();         // Bt Y#0895
-		// Add_Dalgyal();                  // Bt Y#0895
-		// Add_DanseMacabre();             // Bt Y#0895
-		// Add_DeadHand();                 // Ara?
-		// Add_Deadeye();                  // Bt Y#0895
-		// Add_Draugr();                   // Bt Y#0895
-		// Add_DrownedSoul();              // Bt Y#0895
-		// Add_ExplodingPirate();          // Lich underling#7678, then Ara
-		// Add_Family();                   // Catboy Stinkbug#4099
-		// Add_FesteringWretch();          // Bt Y#0895
-		// Add_Flames();                   // Ara
-		// Add_Franknstein();              // vanilla
-		// Add_GhostShip();                // Cevin2006™ (◕‿◕)#7971
-		// Add_Giant();                    // Bt Y#0895
-		// Add_GraveDigger();              // vanilla
-		// Add_HellHound();                // Cevin2006™ (◕‿◕)#7971
-		// Add_Hellhand();                 // Bt Y#0895 
-		// Add_Manananggal();              // Bt Y#0895
-		// Add_Mummy();                    // Bt Y#0895
-		// Add_Obol();                     // Bt Y#0895
-		// Add_PlagueDoctor();             // Cevin2006™ (◕‿◕)#7971
-		// Add_Poltergeist();              // Cevin2006™ (◕‿◕)#7971
-		// Add_Project();                  // Bt Y#0895
-		// Add_PirateCaptainYellowbeard(); // Bt Y#0895
-		// Add_PirateFirstMateSnag();      // Bt Y#0895
-		// Add_PiratePrivateer();          // Bt Y#0895
-		// Add_PirateSwashbuckler();       // Bt Y#0895
-		// Add_Revenant();                 // vanilla
-		// Add_Sarcophagus();              // Bt Y#0895
-		// Add_Skelemagus();               // Cevin2006™ (◕‿◕)#7971
-		// Add_SkeletonArmy();             // LavaErrorDoggo#1564 ?
-		// Add_Summoner();                 // Ara
-		// Add_TombRobber();               // LavaErrorDoggo#1564
-		// Add_Vellum();                   // Bt Y#0895
-		// Add_VengefulSpirit();           // Cevin2006™ (◕‿◕)#7971
-		// Add_WillOTheWisp();             // Bt Y#0895
-		// Add_Zombie();                   // Bt Y#0895
-
-		#endregion
-
-
-		#region Rares
-
-		// Add_Apparition();       // vanilla
-		// Add_BonelordsHorn();    // Cevin2006™ (◕‿◕)#7971
-		// Add_DeadPets();         // Bt Y#0895
-		// Add_DeathKnell();       // Bt Y#0895
-		// Add_DeathKnellBell();   // Bt Y#0895
-		// Add_Dybbuk();           // Bt Y#0895
-		// Add_EmberSpirit();      // Cevin2006™ (◕‿◕)#7971
-		// Add_GhostShipRoyal();   // Cevin2006™ (◕‿◕)#7971
-		// Add_HeadlessHorseman(); // Cevin2006™ (◕‿◕)#7971
-		// Add_Hydra();            // Cevin2006™ (◕‿◕)#7971
-		// Add_Necromancer();      // Bt Y#0895
-		// Add_Ripper();           // Bt Y#0895
-		// Add_ScreamingSkull();   // Bt Y#0895
-		// Add_Silbon();           // Bt Y#0895
-		// Add_SporeDigger();      // LavaErrorDoggo#1564
-		// Add_Wyvern();           // Cevin2006™ (◕‿◕)#7971
-
-		#endregion
+		// Log.LogInfo($"Adding [{allAddMethods.Count(mi => mi.Name.Contains("_Card_"))}] cards.");
 
 		AllGrimoraModCards.Sort((info, cardInfo) => string.Compare(info.name, cardInfo.name, StringComparison.Ordinal));
 		AllPlayableGrimoraModCards = AllGrimoraModCards.Where(info => info.metaCategories.Any()).ToList();
