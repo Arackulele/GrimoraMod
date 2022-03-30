3bd27f73bef5bd3fb206604e60032f4b169c131b
julian-perge <julianperge@gmail.com>
2022-03-29 20:13:19 -0400

    Added Latch Submerge

---
+++ Abilities/Normal/LatchSubmerge.cs
@@ -0,0 +1,21 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public class LatchSubmerge : Latch
+{
+	public static Ability ability;
+	public override Ability Ability => ability;
+	public override Ability LatchAbility => Ability.Submerge;
+}
+
+public partial class GrimoraPlugin
+{
+	public void Add_Ability_LatchSubmerge()
+	{
+		const string rulebookDescription =
+			"When [creature] perishes, its owner chooses a creature to gain the Waterborne sigil.";
+
+		ApiUtils.CreateAbility<LatchSubmerge>(rulebookDescription);
+	}
+}
