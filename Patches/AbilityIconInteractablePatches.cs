2fdcf0a4577dada34f350bb360d578ef422163f0
julian-perge <julianperge@gmail.com>
2022-04-02 04:07:07 -0400

    Cleanup of testing patch

--- Patches/AbilityIconInteractablePatches.cs
+++ Patches/AbilityIconInteractablePatches.cs
@@ -8,56 +8,32 @@ namespace GrimoraMod;
 public class AbilityIconInteractablePatches
 {
 	[HarmonyPrefix, HarmonyPatch(nameof(AbilityIconInteractable.SetFlippedY))]
-	public static bool ChangeStupidNegativeYScalingLogWarning(AbilityIconInteractable __instance, bool flippedY)
+	public static void ChangeStupidNegativeYScalingLogWarning(AbilityIconInteractable __instance, bool flippedY)
 	{
 		if (flippedY)
 		{
-			GrimoraPlugin.Log.LogDebug(
-				$"[AbilityIconPatch.Before] Card [{__instance.card.InfoName()}] Ability [{__instance.Ability}] Is Opponent Card [{__instance.card.OpponentCard}] FlippedY? [{flippedY}]"
-			);
-			GrimoraPlugin.Log.LogDebug(
-				$"[AbilityIconPatch.Before] Card [{__instance.card.InfoName()}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]"
-			);
-			float y = 1f * Mathf.Abs(__instance.transform.localScale.y);
-			__instance.transform.localScale = new Vector3(
-				__instance.transform.localScale.x,
-				y,
-				__instance.transform.localScale.z
-			);
-			__instance.transform.localRotation = Quaternion.Euler(0, 0, 180);
-			// // if (flippedY)
-			// // {
-			// // 	GrimoraPlugin.Log.LogDebug($"[AbilityIconPatch] Card [{__instance.card.name}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]");
-			// // 	__instance.transform.Rotate(new Vector3(0, 0, 180));
-			// // }
-			return false;
+			// float y = 1f * Mathf.Abs(__instance.transform.localScale.y);
+			// __instance.transform.localScale = new Vector3(
+			// 	__instance.transform.localScale.x,
+			// 	y,
+			// 	__instance.transform.localScale.z
+			// );
+
+			if (__instance.gameObject.GetComponent<MeshCollider>().IsNull())
+			{
+				// BoxCollider originalCollider = __instance.GetComponent<BoxCollider>();
+				MeshCollider collider = __instance.gameObject.AddComponent<MeshCollider>();
+				collider.convex = true;
+				collider.sharedMesh = null;
+				collider.sharedMesh = __instance.GetComponent<MeshFilter>().mesh;
+
+				// __instance.coll.bounds.Expand(new Vector3(0.4f, 0.4f, 0.1f));
+				UnityObject.Destroy(__instance.GetComponent<BoxCollider>());
+				__instance.coll = null;
+				__instance.coll = collider;
+			}
 		}
 
-		return true;
-	}
-
-	[HarmonyPostfix, HarmonyPatch(nameof(AbilityIconInteractable.SetFlippedY))]
-	public static void ChangeStupidNegativeYScalingLogWarningAfter(
-		AbilityIconInteractable __instance,
-		bool flippedY
-	)
-	{
-		if (flippedY)
-		{
-			GrimoraPlugin.Log.LogDebug(
-				$"[AbilityIconPatch.After ] Card [{__instance.card.InfoName()}] Ability [{__instance.Ability}] Is Opponent Card [{__instance.card.OpponentCard}] FlippedY? [{flippedY}]"
-			);
-			GrimoraPlugin.Log.LogDebug(
-				$"[AbilityIconPatch.After ] Card [{__instance.card.InfoName()}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]"
-			);
-		}
-		// float y = 1f * Mathf.Abs(__instance.transform.localScale.y);
-		// __instance.transform.localScale = new Vector3(__instance.transform.localScale.x, y, __instance.transform.localScale.z);
-		// // if (flippedY)
-		// // {
-		// // 	GrimoraPlugin.Log.LogDebug($"[AbilityIconPatch] Card [{__instance.card.name}] Rotation [{__instance.transform.rotation}] Local Rotation [{__instance.transform.localRotation}]");
-		// // 	__instance.transform.Rotate(new Vector3(0, 0, 180));
-		// // }
-		// return false;
+		// return true;
 	}
 }
