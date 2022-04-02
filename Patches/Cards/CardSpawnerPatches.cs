9116d3d5b2b411d587a088316a3cc1f332660ce8
julian-perge <julianperge@gmail.com>
2022-04-02 02:16:56 -0400

    Nested if statements so that any further modifications can happen

--- Patches/Cards/CardSpawnerPatches.cs
+++ Patches/Cards/CardSpawnerPatches.cs
@@ -10,28 +10,31 @@ public class CardSpawnerPatches
 	[HarmonyPostfix, HarmonyPatch(nameof(CardSpawner.SpawnPlayableCard))]
 	public static void Postfix(CardInfo info, ref PlayableCard __result)
 	{
-		if (GrimoraSaveUtil.isGrimora && __result.transform.Find("Skeleton2ArmsAttacks").IsNull())
+		if (GrimoraSaveUtil.isGrimora)
 		{
-			Animator skeletonArm2Attacks = UnityObject.Instantiate(
-					AssetUtils.GetPrefab<GameObject>("Skeleton2ArmsAttacks"),
-					__result.transform
-				)
-			 .GetComponent<Animator>();
-			skeletonArm2Attacks.name = "Skeleton2ArmsAttacks";
-			skeletonArm2Attacks.gameObject.AddComponent<AnimMethods>();
-			skeletonArm2Attacks.gameObject.SetActive(false);
-
-			if (info.HasAbility(Ability.Sniper))
+			if (__result.transform.Find("Skeleton2ArmsAttacks").IsNull())
 			{
-				GrimoraPlugin.Log.LogDebug($"Spawning new sentry prefab for card [{info.displayedName}]");
-				GameObject skeletonArmSentry = UnityObject.Instantiate(
-					AssetUtils.GetPrefab<GameObject>("Grimora_Sentry"),
-					__result.transform
-				);
-				skeletonArmSentry.name = "Grimora_Sentry";
-				Transform animObj = skeletonArmSentry.transform.GetChild(0);
-				animObj.gameObject.AddComponent<AnimMethods>();
-				animObj.gameObject.SetActive(false);
+				Animator skeletonArm2Attacks = UnityObject.Instantiate(
+						AssetUtils.GetPrefab<GameObject>("Skeleton2ArmsAttacks"),
+						__result.transform
+					)
+				 .GetComponent<Animator>();
+				skeletonArm2Attacks.name = "Skeleton2ArmsAttacks";
+				skeletonArm2Attacks.gameObject.AddComponent<AnimMethods>();
+				skeletonArm2Attacks.gameObject.SetActive(false);
+
+				if (info.HasAbility(Ability.Sniper))
+				{
+					GrimoraPlugin.Log.LogDebug($"Spawning new sentry prefab for card [{info.displayedName}]");
+					GameObject skeletonArmSentry = UnityObject.Instantiate(
+						AssetUtils.GetPrefab<GameObject>("Grimora_Sentry"),
+						__result.transform
+					);
+					skeletonArmSentry.name = "Grimora_Sentry";
+					Transform animObj = skeletonArmSentry.transform.GetChild(0);
+					animObj.gameObject.AddComponent<AnimMethods>();
+					animObj.gameObject.SetActive(false);
+				}
 			}
 		}
 	}
