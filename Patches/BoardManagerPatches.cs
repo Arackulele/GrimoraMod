7565d2b2deeab6c4ddac1d6e3c8fb3537234953b
julian-perge <julianperge@gmail.com>
2022-04-02 16:40:45 -0400

    Changed CleanUp patch to ClearBoard. Refactored logic to now also check for playable cards to delete the cards with Skin Crawler.

--- Patches/BoardManagerPatches.cs
+++ Patches/BoardManagerPatches.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using DiskCardGame;
 using HarmonyLib;
+using UnityEngine;
 
 namespace GrimoraMod;
 
 [HarmonyPatch(typeof(BoardManager))]
 public class BoardManagerPatches
 {
-	[HarmonyPostfix, HarmonyPatch(nameof(BoardManager.CleanUp))]
+	[HarmonyPostfix, HarmonyPatch(nameof(BoardManager.ClearBoard))]
 	public static IEnumerator CheckForNonCardTriggersOnTheBoardStill(IEnumerator enumerator, BoardManager __instance)
 	{
 		yield return enumerator;
@@ -21,6 +22,13 @@ public class BoardManagerPatches
 				{
 					GrimoraPlugin.Log.LogWarning($"[CleanUp] Destroying NonCardTriggerReceiver [{nonCardTriggerReceiver}] from slot [{slot}]");
 					UnityObject.Destroy(nonCardTriggerReceiver);
+				}
+				var playableCardsNotOnTheBoard = slot.GetComponentsInChildren<PlayableCard>();
+				foreach (var card in playableCardsNotOnTheBoard)
+				{
+					GrimoraPlugin.Log.LogWarning($"[CleanUp] Playing exit for playable card as it exists but not on the board technically [{card}] from slot [{slot}]");
+					card.ExitBoard(0.3f, Vector3.zero);
+					yield return new WaitForSeconds(0.1f);
 				}
 			}
 		}
@@ -70,8 +78,8 @@ public class BoardManagerPatches
 		{
 			__result = new List<CardSlot>();
 			List<CardSlot> slotsWithGiant = __instance.opponentSlots
-				.Where(x => x.Card && x.Card.InfoName() == slot.Card.InfoName())
-				.ToList();
+			 .Where(x => x.Card && x.Card.InfoName() == slot.Card.InfoName())
+			 .ToList();
 
 			if (slotsWithGiant.Count < 2)
 			{
@@ -80,7 +88,7 @@ public class BoardManagerPatches
 			}
 
 			// Log.LogDebug($"Slots that contain [{slot.Card.GetNameAndSlot()}] = {secondSlot.Join(x => x.Index.ToString())}");
-				
+
 			int leftAdjSlotIndex = slotsWithGiant[0].Index - 1;
 			int rightAdjSlotIndex = slotsWithGiant[1].Index + 1;
 			// Log.LogDebug($"Adjacent slot indexes to check left-[{leftAdjSlotIndex}] right-[{rightAdjSlotIndex}]");
@@ -88,6 +96,7 @@ public class BoardManagerPatches
 			{
 				__result.Add(__instance.opponentSlots[leftAdjSlotIndex]);
 			}
+
 			if (__instance.opponentSlots.Exists(x => x.Index == rightAdjSlotIndex && x.Card != slot.Card))
 			{
 				__result.Add(__instance.opponentSlots[rightAdjSlotIndex]);
