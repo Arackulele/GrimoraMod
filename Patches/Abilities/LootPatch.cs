7d4d6eaacc863ac2466a0ea769a9965f814bdfc1
julian-perge <julianperge@gmail.com>
2022-04-05 09:26:02 -0400

    Added patch for Loot to correct visual bug with card not appearing drawn

---
+++ Patches/Abilities/LootPatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using DiskCardGame;
+using HarmonyLib;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(Loot))]
+public class LootPatch
+{
+	[HarmonyPrefix, HarmonyPatch(nameof(Loot.OnDealDamageDirectly))]
+	public static IEnumerator FixVisualDraw(Loot __instance, int amount)
+	{
+		yield return __instance.PreSuccessfulTriggerSequence();
+		bool drawPile3DIsActive = CardDrawPiles3D.Instance && CardDrawPiles3D.Instance.pile;
+		for (int i = 0; i < amount; i++)
+		{
+			if (drawPile3DIsActive)
+			{
+				CardDrawPiles3D.Instance.pile.Draw();
+			}
+			yield return CardDrawPiles.Instance.DrawCardFromDeck();
+		}
+		yield return __instance.LearnAbility();
+	}
+}
