a70380d36d9b64bade46ba8ec9e83fcf98e40726
julian-perge <julianperge@gmail.com>
2022-04-12 19:40:06 -0400

    Added patch for dams to now be the shipwreck pieces.
    Added Grimora themed latch icon.
    
    Refactored AbilitiesUtilPatches to use dictionary instead of if/else

---
+++ Patches/Abilities/CreateDamsPatch.cs
@@ -0,0 +1,32 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(CreateDams))]
+public class CreateDamsPatch
+{
+	[HarmonyPrefix, HarmonyPatch(nameof(CreateDams.SpawnedCardId), MethodType.Getter)]
+	public static bool ChangeDialogue(ref string __result)
+	{
+		if (GrimoraSaveUtil.isNotGrimora)
+		{
+			return true;
+		}
+
+		__result = "Blocked on both sides. No Shipwrecks for the Forgotten Man.";
+		return false;
+	}
+
+	[HarmonyPrefix, HarmonyPatch(nameof(CreateDams.SpawnedCardId), MethodType.Getter)]
+	public static bool ChangeDamsToShipPieces(ref string __result)
+	{
+		if (GrimoraSaveUtil.isNotGrimora)
+		{
+			return true;
+		}
+
+		__result = GrimoraPlugin.NameShipwreckDams;
+		return false;
+	}
+}
