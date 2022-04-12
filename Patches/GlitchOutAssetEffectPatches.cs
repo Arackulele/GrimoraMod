5aa316d230f323e0eaa10fc48c3fa1eb301d2762
julian-perge <julianperge@gmail.com>
2022-04-12 06:53:48 -0400

    fix: Deletion window constantly popping up during glitch out

---
+++ Patches/GlitchOutAssetEffectPatches.cs
@@ -0,0 +1,17 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(GlitchOutAssetEffect))]
+public class GlitchOutAssetEffectPatches
+{
+
+	[HarmonyPrefix, HarmonyPatch(nameof(GlitchOutAssetEffect.ShowDeletionInUI))]
+	public static bool DoNotShowWindowIfDisabled(Transform parent)
+	{
+		return GrimoraSaveUtil.isNotGrimora || GrimoraGameFlowManager.Instance.CurrentGameState != GameState.CardBattle;
+	}
+	
+}
