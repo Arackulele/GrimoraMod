5dc90965dec03ae18d06ce258fe98dbab131d235
julian-perge <julianperge@gmail.com>
2022-04-12 06:28:15 -0400

    Added ForgottenMan, Vampire, BloodGuzzler from contest winners.
    Updated checksum for sprites asset bundle

---
+++ Abilities/Normal/BloodGuzzler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using DiskCardGame;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+public class BloodGuzzler : AbilityBehaviour
+{
+	public static Ability ability;
+	
+	public override Ability Ability => ability;
+
+	private CardModificationInfo _modInfo;
+
+	private void Start()
+	{
+		_modInfo = new CardModificationInfo
+		{
+			nonCopyable = true,
+			singletonId = "grimoramod_BloodGuzzler"
+		};
+		Card.AddTemporaryMod(_modInfo);
+	}
+
+	public override bool RespondsToDealDamage(int amount, PlayableCard target)
+	{
+		return amount > 0;
+	}
+
+	public override IEnumerator OnDealDamage(int amount, PlayableCard target)
+	{
+		yield return PreSuccessfulTriggerSequence();
+		_modInfo.healthAdjustment += amount;
+		Card.OnStatsChanged();
+		Card.Anim.StrongNegationEffect();
+		yield return new WaitForSeconds(0.25f);
+		yield return LearnAbility(0.25f);
+	}
+}
+
+public partial class GrimoraPlugin
+{
+	public void Add_Ability_BloodGuzzler()
+	{
+		AbilityInfo abilityInfo = AbilitiesUtil.GetInfo(Ability.BloodGuzzler);
+		Texture icon = AbilitiesUtil.LoadAbilityIcon(Ability.BloodGuzzler.ToString());
+		AbilityBuilder<BloodGuzzler>.Builder
+		 .SetRulebookDescription(abilityInfo.rulebookDescription)
+		 .SetIcon(icon)
+		 .Build();
+	}
+}
---
+++ Cards/ForgottenMan.cs
@@ -0,0 +1,20 @@
+﻿using DiskCardGame;
+
+namespace GrimoraMod;
+
+public partial class GrimoraPlugin
+{
+	public const string NameForgottenMan = $"{GUID}_ForgottenMan";
+
+	private void Add_Card_ForgottenMan()
+	{
+		CardBuilder.Builder
+			.SetAsNormalCard()
+			.SetAbilities(Ability.CreateDams, Ability.Submerge)
+			.SetBaseAttackAndHealth(1, 2)
+			.SetBoneCost(5)
+			.SetDescription("A waterlogged sailor who wanders the ocean floor and carries his tragic past beside him.")
+			.SetNames(NameForgottenMan, "Forgotten Man")
+			.Build();
+	}
+}
