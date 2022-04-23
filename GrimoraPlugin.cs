cb0df8c7c2c2481012243f10bcfef16a1ae46a8f
Ara <73202349+Arackulele@users.noreply.github.com>
2022-04-23 18:58:50 +0200

    Card move

--- GrimoraPlugin.cs
+++ GrimoraPlugin.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Diagnostics;
 using System.Reflection;
 using BepInEx;
+using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using DiskCardGame;
 using HarmonyLib;
@@ -79,24 +80,141 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 		{
 			yield return new WaitUntil(() => !AllPrefabs.IsNullOrEmpty());
 		}
-	}
+
+		//extra cards dll doesnt actually do anything, as its probably more convenient for us to have everything in the main project
+		if (Chainloader.PluginInfos.ContainsKey("arackulele.inscryption._grimoramodextracards"))
+				{
+			Log.LogDebug($"Detected expansion mod");
+			Add_Card_BalBal();
+			Add_Card_BloodySack();
+			Add_Card_BoneCollective();
+			Add_Card_CompoundFracture();
+			Add_Card_DisturbedGrave();
+			Add_Card_EmberSpirit();
+			Add_Card_FesteringWretch();
+			Add_Card_Flameskull();
+			Add_Card_Haltia();
+			Add_Card_HauntedMirror();
+			Add_Card_IceCube();
+			Add_Card_LaLlorona();
+			Add_Card_Moroi();
+			Add_Card_Nixie();
+			Add_Card_Nosferat();
+			Add_Card_PiratePolly();
+			Add_Card_PiratePrivateer();
+			Add_Card_PossessedArmour();
+			Add_Card_Rot();
+			Add_Card_Warthr();
+
+			Add_Card_Fylgja();
+			Add_Card_Wyvern();
+			Add_Card_Wight();
+			Add_Card_SkeletonArmy();
+				}
+		}
 
 	private void LoadAbilitiesAndCards()
 	{
 		Stopwatch stopwatch = Stopwatch.StartNew();
 		Log.LogDebug($"[LoadAbilitiesAndCards] Loading cards...");
 
-		// What this does, is that every method that exists under this partial class, Grimora Plugin,
-		//	will be searched for and of the ones that start with 'Add_', will be invoked all at once after sorting by name.
-		// We sort by name so Abilities come first because abilities have their method name like 'Add_Ability_' while cards have 'Add_Card_'
+		// Sort and add all abilities, since cards are split its faster to just add them manually than to save a few lines of code with a blacklist (also its nice for organization)
+
 		var allAddMethods = AccessTools.GetDeclaredMethods(typeof(GrimoraPlugin))
-		 .Where(mi => mi.Name.StartsWith("Add_"))
+		 .Where(mi => mi.Name.StartsWith("Add_Ability"))
 		 .ToList();
 		allAddMethods.Sort((mi, mi2) => string.Compare(mi.Name, mi2.Name, StringComparison.Ordinal));
 		allAddMethods.ForEach(mi => mi.Invoke(this, null));
-		// Log.LogInfo($"Adding [{allAddMethods.Count(mi => mi.Name.Contains("_Card_"))}] cards.");
 
-		AllGrimoraModCards.Sort((info, cardInfo) => string.Compare(info.name, cardInfo.name, StringComparison.Ordinal));
+		//Add Cards, this was automatic but i made it manual so i can more easily split the cards
+		//Will add the individual Art Credits we had once later here, nice to have that organized so we know who did what
+		#region Cards
+
+		Add_Card_Animator();
+		Add_Card_Apparition();
+		Add_Card_Banshee();
+		Add_Card_Bonehound();
+		Add_Card_Bonelord();
+		Add_Card_BonelordsHorn();
+		Add_Card_Bonepile();
+		Add_Card_BonePrince();
+		Add_Card_BooHag();
+		Add_Card_CalaveraCatrina();
+		Add_Card_Catacomb();
+		Add_Card_Centurion();
+		Add_Card_Dalgyal();
+		Add_Card_DanseMacabre();
+		Add_Card_Deadeye();
+		Add_Card_DeadHand();
+		Add_Card_DeadPets();
+		Add_Card_DeadTree();
+		Add_Card_DeathKnell();
+		Add_Card_Doll();
+		Add_Card_Draugr();
+		Add_Card_DrownedSoul();
+		Add_Card_Dybbuk();
+		Add_Card_Family();
+		Add_Card_Flames();
+		Add_Card_ForgottenMan();
+		Add_Card_Franknstein();
+		Add_Card_Gashadokuro_Stage1_MassGrave();
+		Add_Card_Gashadokuro_Stage2_RisingHunger();
+		Add_Card_Gashadokuro_Stage3();
+		Add_Card_GhostShip();
+		Add_Card_GhostShipRoyal();
+		Add_Card_Giant();
+		Add_Card_Glacier_Stage1();
+		Add_Card_Glacier_Stage2_FrostGiant();
+		Add_Card_Gravebard();
+		Add_Card_Gravedigger();
+		Add_Card_Haltia();
+		Add_Card_HeadlessHorseman();
+		Add_Card_Hellhand();
+		Add_Card_HellHound();
+		Add_Card_Hydra();
+		Add_Card_Jikininki();
+		Add_Card_Kennel();
+		Add_Card_Manananggal();
+		Add_Card_Mummy();
+		Add_Card_Necromancer();
+		Add_Card_Obol();
+		Add_Card_PirateCaptainYellowbeard();
+		Add_Card_PirateExploding();
+		Add_Card_PirateFirstMateSnag();
+		Add_Card_PirateSwashbuckler();
+		Add_Card_PlagueDoctor();
+		Add_Card_Poltergeist();
+		Add_Card_Project();
+		Add_Card_Revenant();
+		Add_Card_Ripper();
+		Add_Card_RotTail();
+		Add_Card_Sarcophagus();
+		Add_Card_ScreamingSkull();
+		Add_Card_Shipwreck();
+		Add_Card_Shipwreck_Dams();
+		Add_Card_Silbon();
+
+		Add_Card_Skelemagus();
+		Add_Card_Sluagh();
+		Add_Card_Spectrabbit();
+		Add_Card_Sporedigger();
+		Add_Card_StarvedMan();
+		Add_Card_Summoner();
+		Add_Card_TamperedCoffin();
+		Add_Card_TombRobber();
+		Add_Card_Vampire();
+		Add_Card_Vellum();
+		Add_Card_VengefulSpirit();
+		Add_Card_WardingPresence();
+		Add_Card_Wechuge();
+		Add_Card_WillOTheWisp();
+		Add_Card_Writher();
+		Add_Card_WritherTail();
+		Add_Card_Zombie();
+				#endregion
+				// Log.LogInfo($"Adding [{allAddMethods.Count(mi => mi.Name.Contains("_Card_"))}] cards.");
+
+				AllGrimoraModCards.Sort((info, cardInfo) => string.Compare(info.name, cardInfo.name, StringComparison.Ordinal));
 		AllPlayableGrimoraModCards = AllGrimoraModCards.Where(info => info.metaCategories.Any()).ToList();
 
 		// change just the artwork of Starvation
