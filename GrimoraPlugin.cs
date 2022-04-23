b108dcac84b489b1f67d87d205a6786725538b32
julian-perge <julianperge@gmail.com>
2022-04-23 19:21:50 -0400

    Cleaned up card and abilities additions

--- GrimoraPlugin.cs
+++ GrimoraPlugin.cs
@@ -50,14 +50,26 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 
 		ConfigHelper.Instance.BindConfig();
 
+		LoadAssetsSync();
+
+		StartCoroutine(LoadAssetsAsync());
+	}
+
+	private void LoadAssetsSync()
+	{
 		AllAbilitiesTextures = AssetUtils.LoadAssetBundle<Texture>("grimoramod_abilities");
 		AllControllers = AssetUtils.LoadAssetBundle<RuntimeAnimatorController>("grimoramod_controller");
 		AllMats = AssetUtils.LoadAssetBundle<Material>("grimoramod_mats");
 		AllSounds = AssetUtils.LoadAssetBundle<AudioClip>("grimoramod_sounds");
 		AllSprites = AssetUtils.LoadAssetBundle<Sprite>("grimoramod_sprites");
 		AllMesh = AssetUtils.LoadAssetBundle<Mesh>("grimoramod_mesh");
+	}
 
-		StartCoroutine(LoadAssetsAsync());
+	private IEnumerator LoadAssetsAsync()
+	{
+		Log.LogInfo($"Loading asset bundles");
+
+		yield return StartCoroutine(AssetUtils.LoadAssetBundleAsync<GameObject>("grimoramod_prefabs"));
 	}
 
 	// private IEnumerator HotReloadMenuCardAdd()
@@ -78,143 +90,122 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 
 		if (AllPrefabs.IsNullOrEmpty())
 		{
+			Log.LogWarning($"Prefabs are still being loaded, waiting until they have finished.");
 			yield return new WaitUntil(() => !AllPrefabs.IsNullOrEmpty());
 		}
-
-		//extra cards dll doesnt actually do anything, as its probably more convenient for us to have everything in the main project
-		if (Chainloader.PluginInfos.ContainsKey("arackulele.inscryption._grimoramodextracards"))
-				{
-			Log.LogDebug($"Detected expansion mod");
-			Add_Card_BalBal();
-			Add_Card_BloodySack();
-			Add_Card_BoneCollective();
-			Add_Card_CompoundFracture();
-			Add_Card_DisturbedGrave();
-			Add_Card_EmberSpirit();
-			Add_Card_FesteringWretch();
-			Add_Card_Flameskull();
-			Add_Card_Haltia();
-			Add_Card_HauntedMirror();
-			Add_Card_IceCube();
-			Add_Card_LaLlorona();
-			Add_Card_Moroi();
-			Add_Card_Nixie();
-			Add_Card_Nosferat();
-			Add_Card_PiratePolly();
-			Add_Card_PiratePrivateer();
-			Add_Card_PossessedArmour();
-			Add_Card_Rot();
-			Add_Card_Warthr();
-
-			Add_Card_Fylgja();
-			Add_Card_Wyvern();
-			Add_Card_Wight();
-			Add_Card_SkeletonArmy();
-				}
-		}
+	}
+	
+	private void LoadAbilities()
+	{
+		// Since cards are split, abilities will be added like this to save some lines
+		AccessTools.GetDeclaredMethods(typeof(GrimoraPlugin))
+		 .Where(mi => mi.Name.StartsWith("Add_Ability"))
+		 .ForEach(mi => mi.Invoke(this, null));
+	}
 
 	private void LoadAbilitiesAndCards()
 	{
 		Stopwatch stopwatch = Stopwatch.StartNew();
+
+		LoadAbilities();
+		// Manually adding so splitting cards is easier for future use
 		Log.LogDebug($"[LoadAbilitiesAndCards] Loading cards...");
 
-		// Sort and add all abilities, since cards are split its faster to just add them manually than to save a few lines of code with a blacklist (also its nice for organization)
+		#region Normal
 
-		var allAddMethods = AccessTools.GetDeclaredMethods(typeof(GrimoraPlugin))
-		 .Where(mi => mi.Name.StartsWith("Add_Ability"))
-		 .ToList();
-		allAddMethods.Sort((mi, mi2) => string.Compare(mi.Name, mi2.Name, StringComparison.Ordinal));
-		allAddMethods.ForEach(mi => mi.Invoke(this, null));
+		Add_Card_Banshee();                   // vanilla
+		Add_Card_Bonehound();                 // vanilla
+		Add_Card_Bonelord();                  // Ryan S. Art
+		Add_Card_Bonepile();                  // Bt Y#0895
+		Add_Card_BonePrince();                // Cevin2006™ (◕‿◕)#7971
+		Add_Card_BooHag();                    // Bt Y#0895
+		Add_Card_Catacomb();                  // Bt Y#0895
+		Add_Card_Centurion();                 // Bt Y#0895
+		Add_Card_Dalgyal();                   // Bt Y#0895
+		Add_Card_DanseMacabre();              // Bt Y#0895
+		Add_Card_Deadeye();                   // Bt Y#0895
+		Add_Card_DeadHand();                  // Bt Y#0895
+		Add_Card_DeadTree();                  // Bt Y#0895
+		Add_Card_Doll();                      // Bt Y#0895
+		Add_Card_Draugr();                    // Bt Y#0895
+		Add_Card_DrownedSoul();               // Bt Y#0895
+		Add_Card_Family();                    // Catboy Stinkbug#4099
+		Add_Card_Flames();                    // Cevin2006™ (◕‿◕)#7971
+		Add_Card_ForgottenMan();              // Anne Bean
+		Add_Card_Franknstein();               // vanilla
+		Add_Card_GhostShip();                 // Cevin2006™ (◕‿◕)#7971
+		Add_Card_Giant();                     // Bt Y#0895
+		Add_Card_TwinGiant_Ephialtes();       // Bt Y#0895
+		Add_Card_TwinGiant_Otis();            // Bt Y#0895
+		Add_Card_Glacier_Stage1();            // Bt Y#0895
+		Add_Card_Glacier_Stage2_FrostGiant(); // Bt Y#0895
+		Add_Card_Gravebard();                 // Bt Y#0895
+		Add_Card_Gravedigger();               // vanilla
+		Add_Card_Hellhand();                  // Bt Y#0895
+		Add_Card_HellHound();                 // Cevin2006™ (◕‿◕)#7971
+		Add_Card_Jikininki();                 // Bt Y#0895
+		Add_Card_Kennel();                    // Bt Y#0895
+		Add_Card_Manananggal();               // Bt Y#0895
+		Add_Card_Mummy();                     // Bt Y#0895
+		Add_Card_Obol();                      // Bt Y#0895
+		Add_Card_PirateExploding();           // Lich underling#7678, then Ara
+		Add_Card_PirateFirstMateSnag();       // Bt Y#0895
+		Add_Card_PirateSwashbuckler();        // Bt Y#0895
+		Add_Card_PlagueDoctor();              // Cevin2006™ (◕‿◕)#7971
+		Add_Card_Poltergeist();               // Cevin2006™ (◕‿◕)#7971
+		Add_Card_Project();                   // Bt Y#0895
+		Add_Card_Revenant();                  // vanilla
+		Add_Card_RotTail();                   // Bt Y#0895
+		Add_Card_Sarcophagus();               // Bt Y#0895
+		Add_Card_Shipwreck();                 // Bt Y#0895
+		Add_Card_Shipwreck_Dams();            // Anne Bean
+		Add_Card_Skelemagus();                // Cevin2006™ (◕‿◕)#7971
+		Add_Card_Sluagh();                    // Bt Y#0895
+		Add_Card_Spectrabbit();               // Bt Y#0895
+		Add_Card_Summoner();                  // Cevin2006™ (◕‿◕)#7971
+		Add_Card_TamperedCoffin();            // Bt Y#0895
+		Add_Card_TombRobber();                // LavaErrorDoggo#1564
+		Add_Card_Vampire();                   // gabe
+		Add_Card_Vellum();                    // Bt Y#0895
+		Add_Card_VengefulSpirit();            // Cevin2006™ (◕‿◕)#7971
+		Add_Card_WardingPresence();           // Bt Y#0895
+		Add_Card_Wechuge();                   // Bt Y#0895
+		Add_Card_WillOTheWisp();              // Bt Y#0895
+		Add_Card_Zombie();                    // Bt Y#0895
 
-		//Add Cards, this was automatic but i made it manual so i can more easily split the cards
-		//Will add the individual Art Credits we had once later here, nice to have that organized so we know who did what
-		#region Cards
+		#endregion
 
-		Add_Card_Animator();
-		Add_Card_Apparition();
-		Add_Card_Banshee();
-		Add_Card_Bonehound();
-		Add_Card_Bonelord();
-		Add_Card_BonelordsHorn();
-		Add_Card_Bonepile();
-		Add_Card_BonePrince();
-		Add_Card_BooHag();
-		Add_Card_CalaveraCatrina();
-		Add_Card_Catacomb();
-		Add_Card_Centurion();
-		Add_Card_Dalgyal();
-		Add_Card_DanseMacabre();
-		Add_Card_Deadeye();
-		Add_Card_DeadHand();
-		Add_Card_DeadPets();
-		Add_Card_DeadTree();
-		Add_Card_DeathKnell();
-		Add_Card_Doll();
-		Add_Card_Draugr();
-		Add_Card_DrownedSoul();
-		Add_Card_Dybbuk();
-		Add_Card_Family();
-		Add_Card_Flames();
-		Add_Card_ForgottenMan();
-		Add_Card_Franknstein();
-		Add_Card_Gashadokuro_Stage1_MassGrave();
-		Add_Card_Gashadokuro_Stage2_RisingHunger();
-		Add_Card_Gashadokuro_Stage3();
-		Add_Card_GhostShip();
-		Add_Card_GhostShipRoyal();
-		Add_Card_Giant();
-		Add_Card_Glacier_Stage1();
-		Add_Card_Glacier_Stage2_FrostGiant();
-		Add_Card_Gravebard();
-		Add_Card_Gravedigger();
-		Add_Card_Haltia();
-		Add_Card_HeadlessHorseman();
-		Add_Card_Hellhand();
-		Add_Card_HellHound();
-		Add_Card_Hydra();
-		Add_Card_Jikininki();
-		Add_Card_Kennel();
-		Add_Card_Manananggal();
-		Add_Card_Mummy();
-		Add_Card_Necromancer();
-		Add_Card_Obol();
-		Add_Card_PirateCaptainYellowbeard();
-		Add_Card_PirateExploding();
-		Add_Card_PirateFirstMateSnag();
-		Add_Card_PirateSwashbuckler();
-		Add_Card_PlagueDoctor();
-		Add_Card_Poltergeist();
-		Add_Card_Project();
-		Add_Card_Revenant();
-		Add_Card_Ripper();
-		Add_Card_RotTail();
-		Add_Card_Sarcophagus();
-		Add_Card_ScreamingSkull();
-		Add_Card_Shipwreck();
-		Add_Card_Shipwreck_Dams();
-		Add_Card_Silbon();
+		#region Rares
 
-		Add_Card_Skelemagus();
-		Add_Card_Sluagh();
-		Add_Card_Spectrabbit();
-		Add_Card_Sporedigger();
-		Add_Card_StarvedMan();
-		Add_Card_Summoner();
-		Add_Card_TamperedCoffin();
-		Add_Card_TombRobber();
-		Add_Card_Vampire();
-		Add_Card_Vellum();
-		Add_Card_VengefulSpirit();
-		Add_Card_WardingPresence();
-		Add_Card_Wechuge();
-		Add_Card_WillOTheWisp();
-		Add_Card_Writher();
-		Add_Card_WritherTail();
-		Add_Card_Zombie();
-				#endregion
-				// Log.LogInfo($"Adding [{allAddMethods.Count(mi => mi.Name.Contains("_Card_"))}] cards.");
+		Add_Card_Animator();                        // Bt Y#0895
+		Add_Card_Apparition();                      // Bt Y#0895
+		Add_Card_BonelordsHorn();                   // Cevin2006™ (◕‿◕)#7971
+		Add_Card_CalaveraCatrina();                 // Bt Y#0895
+		Add_Card_DeadPets();                        // Bt Y#0895
+		Add_Card_DeathKnell();                      // Bt Y#0895
+		Add_Card_DeathKnellBell();                  // Bt Y#0895
+		Add_Card_Dybbuk();                          // Bt Y#0895
+		Add_Card_Gashadokuro_Stage1_MassGrave();    // Bt Y#0895
+		Add_Card_Gashadokuro_Stage2_RisingHunger(); // Bt Y#0895
+		Add_Card_Gashadokuro_Stage3();              // Bt Y#0895
+		Add_Card_GhostShipRoyal();                  // Cevin2006™ (◕‿◕)#7971
+		Add_Card_HeadlessHorseman();                // Cevin2006™ (◕‿◕)#7971
+		Add_Card_Hydra();                           // Cevin2006™ (◕‿◕)#7971
+		Add_Card_Necromancer();                     // Bt Y#0895
+		Add_Card_PirateCaptainYellowbeard();        // Bt Y#0895
+		Add_Card_Ripper();                          // Bt Y#0895
+		Add_Card_ScreamingSkull();                  // Bt Y#0895
+		Add_Card_Silbon();                          // Bt Y#0895
+		Add_Card_Sporedigger();                     // LavaErrorDoggo#1564
+		Add_Card_StarvedMan();                      // ~=Lost Soul=~
+		Add_Card_Writher();                         // Bt Y#0895
+		Add_Card_WritherTail();                     // Bt Y#0895
 
-				AllGrimoraModCards.Sort((info, cardInfo) => string.Compare(info.name, cardInfo.name, StringComparison.Ordinal));
+		#endregion
+		
+		LoadExpansionCards();
+
+		AllGrimoraModCards.Sort((info, cardInfo) => string.Compare(info.name, cardInfo.name, StringComparison.Ordinal));
 		AllPlayableGrimoraModCards = AllGrimoraModCards.Where(info => info.metaCategories.Any()).ToList();
 
 		// change just the artwork of Starvation
@@ -222,19 +213,69 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 		card.portraitTex = AllSprites.Single(sp => sp.name.Equals("Starvation"));
 		card.portraitTex.RegisterEmissionForSprite(AllSprites.Single(sp => sp.name.Equals("Starvation_emission")));
 
+		AddDebugCards();
+
+		stopwatch.Stop();
+		Log.LogDebug($"[LoadAbilitiesAndCards] Finished loading all abilities and cards in [{stopwatch.ElapsedMilliseconds}]ms");
+	}
+	
+	private void LoadExpansionCards()
+	{
+		//extra cards dll doesnt actually do anything, as its probably more convenient for us to have everything in the main project
+		if (Chainloader.PluginInfos.ContainsKey("arackulele.inscryption._grimoramodextracards"))
+		{
+			Log.LogInfo($"Detected expansion mod, adding a new set of cards!");
+
+			#region Normal
+
+			Add_Card_BloodySack();       // Bt Y#0895
+			Add_Card_CompoundFracture(); // Bt Y#0895
+			Add_Card_DisturbedGrave();   // Bt Y#0895
+			Add_Card_FesteringWretch();  // Bt Y#0895
+			Add_Card_Flameskull();       // Bt Y#0895
+			Add_Card_Haltia();           // Bt Y#0895
+			Add_Card_HauntedMirror();    // Bt Y#0895
+			// Add_Card_IceCube();          // Bt Y#0895
+			Add_Card_LaLlorona();       // Bt Y#0895
+			Add_Card_Moroi();           // Bt Y#0895
+			Add_Card_Nixie();           // Bt Y#0895
+			Add_Card_PiratePolly();     // Bt Y#0895
+			Add_Card_PiratePrivateer(); // Bt Y#0895
+			Add_Card_PossessedArmour(); // Bt Y#0895
+			Add_Card_Rot();             // Bt Y#0895
+			Add_Card_SkeletonArmy();    // Cevin2006™ (◕‿◕)#7971
+			Add_Card_Warthr();          // Bt Y#0895
+			Add_Card_Wight();           // Bt Y#0895
+
+			#endregion
+
+			#region Rares
+
+			Add_Card_BalBal();         // Bt Y#0895
+			Add_Card_BoneCollective(); // Bt Y#0895
+			Add_Card_EmberSpirit();    // Cevin2006™ (◕‿◕)#7971
+			Add_Card_Fylgja();         // Bt Y#0895
+			Add_Card_Nosferat();       // Bt Y#0895
+			Add_Card_Wyvern();         // Cevin2006™ (◕‿◕)#7971
+
+			#endregion
+		}
+	}
+
+	private void AddDebugCards()
+	{
+		CardInfo trapInfo = "Trap".GetCardInfo();
 		CardBuilder.Builder
 		 .SetAbilities(Ability.BoneDigger, Ability.SteelTrap, Haunter.ability)
 		 .SetBaseAttackAndHealth(0, 1)
-		 .SetNames($"{GUID}_!TRAP", "!TEST Trap", "Trap".GetCardInfo().portraitTex)
+		 .SetNames($"{GUID}_!TRAP", "!TEST Trap", trapInfo.portraitTex)
 		 .Build();
 
 		CardBuilder.Builder
 		 .SetAbilities(Ability.DeathShield)
 		 .SetBaseAttackAndHealth(0, 99)
-		 .SetNames($"{GUID}_!BLOCKER", "!TEST Blocker", "Trap".GetCardInfo().portraitTex)
+		 .SetNames($"{GUID}_!BLOCKER", "!TEST Blocker", trapInfo.portraitTex)
 		 .Build();
-		stopwatch.Stop();
-		Log.LogDebug($"[LoadAbilitiesAndCards] Finished loading all abilities and cards in [{stopwatch.ElapsedMilliseconds}]ms");
 	}
 
 	private void OnDestroy()
@@ -242,6 +283,7 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 		AllAbilitiesTextures = null;
 		AllControllers = null;
 		AllMats = null;
+		AllMesh = null;
 		AllPrefabs = null;
 		AllSprites = null;
 		AllSounds = null;
@@ -278,12 +320,5 @@ public partial class GrimoraPlugin : BaseUnityPlugin
 		}
 
 		Destroy(gameObject, 6f);
-	}
-
-	private IEnumerator LoadAssetsAsync()
-	{
-		Log.LogInfo($"Loading asset bundles");
-
-		yield return StartCoroutine(AssetUtils.LoadAssetBundleAsync<GameObject>("grimoramod_prefabs"));
 	}
 }
