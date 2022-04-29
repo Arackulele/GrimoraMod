3ca68158a8d4cb2bffa2a1f49d90787dcab5b578
julian-perge <julianperge@gmail.com>
2022-04-28 21:58:27 -0400

    Added patch for activated heal ability to only activate if current health is less than the max health

---
+++ Patches/Abilities/ActivatedHealPatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GrimoraMod;
+
+[HarmonyPatch(typeof(ActivatedHeal))]
+public class ActivatedHealPatch
+{
+	[HarmonyPostfix, HarmonyPatch(nameof(ActivatedHeal.Activate))]
+	public static IEnumerator FixLogicForHealing(IEnumerator enumerator, ActivatedHeal __instance)
+	{
+		int maxHealth = __instance.Card.MaxHealth;
+		int currentHealth = __instance.Card.Health;
+		if (__instance.Card && maxHealth > currentHealth)
+		{
+			__instance.Card.HealDamage(maxHealth - currentHealth);
+			yield return new WaitForSeconds(0.25f);
+		}
+	}
+}
