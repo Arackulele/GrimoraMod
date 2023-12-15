7941a150f8a4b07f727e1adb66f5d3de88590b7a
SarahKirksey <26732456+SarahKirksey@users.noreply.github.com>
2023-12-15 12:35:39 -0800

    Shipwrecks? Flotsam.

--- Abilities/Normal/CreateShipwrecks.cs
+++ Abilities/Normal/CreateShipwrecks.cs
@@ -10,6 +10,7 @@ public class CreateShipwrecks : CreateCardsAdjacent
 
 	public override string SpawnedCardId => GrimoraPlugin.NameShipwreckDams;
 
+	// TODO: Reconsider this line, and possibly the name of the sigil
 	public override string CannotSpawnDialogue => "Blocked on both sides. No Shipwrecks for the Forgotten Man.";
 
 }
@@ -19,7 +20,7 @@ public partial class GrimoraPlugin
 	public void Add_Ability_CreateShipwrecks()
 	{
 		const string rulebookDescription =
-			$"When [creature] is played, a Shipwreck is created on each empty adjacent space. [define:{NameShipwreckDams}]";
+			$"When [creature] is played, a Flotsam is created on each empty adjacent space. [define:{NameShipwreckDams}]";
 
 		AbilityBuilder<CreateShipwrecks>.Builder
 		 .SetIcon(AbilitiesUtil.LoadAbilityIcon(Ability.CreateDams.ToString()))
